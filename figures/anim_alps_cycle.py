b46836ae17bdb8ac24988771ee3831a3d1f2c727
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-03-02 16:53:15 +0100

    Add Natural Earth vectors on animation.

--- figures/anim_alps_cycle.py
+++ figures/anim_alps_cycle.py
@@ -8,9 +8,26 @@ sys.path.append('iceplotlib')
 import iceplotlib.plot as iplt
 from matplotlib.colors import LogNorm
 from matplotlib.animation import FuncAnimation
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
 velonorm = LogNorm(1e1, 1e3)
 
+# cartopy features
+rivers = cfeature.NaturalEarthFeature(
+    category='physical', name='rivers_lake_centerlines', scale='10m',
+    edgecolor='0.25', facecolor='none', lw=0.5)
+lakes = cfeature.NaturalEarthFeature(
+    category='physical', name='lakes', scale='10m',
+    edgecolor='0.25', facecolor='0.85', lw=0.25)
+coastline = cfeature.NaturalEarthFeature(
+    category='physical', name='coastline', scale='10m',
+    edgecolor='0.25', facecolor='none', lw=0.25)
+graticules = cfeature.NaturalEarthFeature(
+    category='physical', name='graticules_1', scale='10m',
+    edgecolor='0.25', facecolor='none', lw=0.1)
+
+# drawing function
 def draw(t, ax, cursor):
     """What to draw at each animation step."""
     age = -t/1e3
@@ -18,13 +35,19 @@ def draw(t, ax, cursor):
     ax.cla()
 
     # plot
-    im = nc.imshow('topg', ax, t, vmin=0.0, vmax=3e3, cmap='Greys')
+    im = nc.imshow('topg', ax, t, vmin=0.0, vmax=3e3, cmap='Greys', zorder=-1)
     im = nc.imshow('thk', ax, t, vmin=0.0, vmax=3e3,
                    cmap='Blues_r', alpha=0.75)
     cs = nc.contour('usurf', ax, t, levels=range(0, 4000, 1000),
                     colors='k', linewidths=0.25)
     cs = nc.icemargin(ax, t, linewidths=0.5)
     ax.text(0.05, 0.90, '%.1f ka' % age, transform=ax.transAxes)
+
+    # add cartopy vectors
+    ax.add_feature(rivers, zorder=0)
+    ax.add_feature(lakes, zorder=0)
+    ax.add_feature(coastline, zorder=0)
+    ax.add_feature(graticules)
 
     # update cursor
     cursor.set_data(age, (0, 1))
@@ -34,13 +57,14 @@ def draw(t, ax, cursor):
 
 # initialize figure
 figw, figh = 135.01, 120.01
-fig, ax = iplt.subplots_mm(figsize=(figw, figh),
+fig, ax = iplt.subplots_mm(figsize=(figw, figh), projection=ccrs.UTM(32),
                            left=2.5, right=20.0, bottom=42.5, top=2.5)
 tsax = fig.add_axes([2.5/figw, 10.0/figh, 1-15.0/figw, 30.0/figh])
 cax = fig.add_axes([1-17.5/figw, 42.5/figh, 5.0/figw, 1-45.0/figh])
 
 # add signature
-fig.text(1-2.5/figw, 2.5/figh, 'J. Seguinot (2016)', ha='right', va='bottom')
+fig.text(1-2.5/figw, 2.5/figh, 'J. Seguinot et al. (2016)',
+         ha='right', va='bottom')
 
 # load time series data
 filepath = ('/home/juliens/pism/output/0.7.3/alps-wcnn-5km/'
