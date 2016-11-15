de106f0d37e7a5afbadca4702ae49689852659d7
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-11-15 16:48:09 +0100

    Add and combine multiple SRTM tiles.
    This will allow higher resolution than ETOPO1.

--- data/retrieve-external.sh
+++ data/retrieve-external.sh
@@ -31,24 +31,33 @@ gdalwarp -s_srs EPSG:4326 -t_srs EPSG:32632 -r bilinear \
          $dest etopo1-alps.nc
 
 # SRTM original cell-registered data
-orig=http://www.ngdc.noaa.gov/mgg/global/relief/ETOPO1/data/bedrock/\
-cell_registered/georeferenced_tiff/ETOPO1_Bed_c_geotiff.zip
-dest=etopo1-world.tif
-orig="http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/srtm_38_03.zip"
-dest=srtm_38_03.tif
-if [ ! -f "$dest" ]
-then
-    wget $orig -O ${dest%.tif}.zip
-    unzip -n ${dest%.tif}.zip ${dest%.tif}.???
-    rm ${dest%.tif}.zip
-fi
+root=http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/
+for tile in srtm_{37..41}_{03..04}
+do
+    if [ ! -f $tile.tif ]
+    then
+        wget $root/$tile.zip -O $tile.zip
+        unzip -n $tile.zip $tile.{hdr,tfw,tif}
+        rm $tile.zip
+    fi
+done
 
-# reproject to UTM 32
+# SRTM mosaic vrt
+gdalbuildvrt srtm.vrt srtm_??_??.tif
+
+# SRTM UTM 32 entire Alps, 1500x1000 km, 1000 m
 gdalwarp -s_srs EPSG:4326 -t_srs EPSG:32632 -r bilinear \
-         -te 0 4500000 1500000 5500000 -tr 250 250 \
-         -srcnodata -2147483648 -dstnodata -32768 \
+         -te 0 4500000 1500000 5500000 -tr 1000 1000 \
+         -srcnodata -32768 -dstnodata -32768 \
+         -wm 1024 -wo SOURCE_EXTRA=100 -of netcdf -overwrite \
+         srtm.vrt srtm-alps.nc
+
+# SRTM UTM 32 Western Alps, 315x210 km, 175 m
+gdalwarp -s_srs EPSG:4326 -t_srs EPSG:32632 -r bilinear \
+         -te 180000 5040000 495000 5250000 -tr 175 175 \
+         -srcnodata -32768 -dstnodata -32768 \
          -wm 512 -wo SOURCE_EXTRA=100 -of netcdf -overwrite \
-         $dest srtm-alps.nc
+         srtm.vrt srtm-west.nc
 
 # Swisstopo Geology 500
 orig=http://data.geo.admin.ch/ch.swisstopo.geologie-geologische_karte/data.zip
