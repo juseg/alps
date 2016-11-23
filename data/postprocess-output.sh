eaf7bb008e06dbc89bb41e59fa95ae41ca04fac1
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-11-23 17:12:25 +0100

    Update output postprocessing script.

--- data/postprocess-output.sh
+++ data/postprocess-output.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-ifile=~/pism/output/0.7.3/alps-wcnn-1km/epica3222cool0950+acyc1+esia5/y0099000-extra.nc
-ofile=alps-wcnn-1km-21ka.nc
-
 # make directory or update modification date
 mkdir -p processed
 touch processed
 cd processed
 
-# extract topographies time slice
-ncks -O -v thk,topg,usurf -d time,4 $ifile $ofile
+# Post-process 1km extra dataset
+# ------------------------------
+
+# pism output file
+odir=$HOME/pism/output/0.7.3
+runname=alps-wcnn-1km/epica3222cool0950+acyc1+esia5
+ifile=$odir/$runname/extra.nc
+ofile=${runname/"/"/-}-21ka.nc
+vars=thk,topg,usurf
+
+# extract 21ka time slice
+echo "preparing $ofile ..."
+ncks -O -v $vars -d time,989 $ifile $ofile
 
 # add thickness mask
 ncap2 -A -s "mask=byte(thk>1)" $ofile
