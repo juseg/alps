986b1a486dbed334df083603aba594446beec1ac
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-11-28 15:33:14 +0100

    Add duration GTiff+Shp postprocessing script.

---
+++ data/gtif_duration.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import os
+import numpy as np
+from netCDF4 import Dataset
+from osgeo import gdal
+from osgeo import ogr
+from osgeo import osr
+
+# file paths
+outdir = os.environ['HOME'] + '/pism/output/0.7.3/'
+runname = 'alps-wcnn-1km/epica3222cool0950+acyc1+esia5'
+varname = 'duration'
+ifilepath = outdir + runname + '/extra.nc'
+ofilepath = 'processed/%s-%s' % (runname.replace('/', '-'), varname)
+
+# read extra output
+nc = Dataset(ifilepath)
+x = nc.variables['x'][:]
+y = nc.variables['y'][:]
+thk = nc.variables['thk'][:]
+nc.close()
+
+# get grid size and origin
+cols = len(x)
+rows = len(y)
+dx = x[1] - x[0]
+dy = y[1] - y[0]
+x0 = x[0] - dx/2
+y0 = y[0] - dy/2
+x1 = x[-1] + dx/2
+y1 = y[-1] + dy/2
+
+# compute duration of ice cover
+duration = (thk >= 1.0).sum(axis=0)*100.0
+
+# spatial reference system
+srs = osr.SpatialReference()
+srs.ImportFromEPSG(32632)
+
+# generate geotiff
+driver = gdal.GetDriverByName('GTiff')
+rast = driver.Create(ofilepath + '.tif', cols, rows, 1, gdal.GDT_Float32)
+rast.SetGeoTransform((x0, dx, 0, y1, 0, -dy))
+rast.SetProjection(srs.ExportToWkt())
+band = rast.GetRasterBand(1)
+band.WriteArray(np.flipud(duration.T))
+band.ComputeStatistics(0)
+band.FlushCache()
+
+# generate contours
+#ContourGenerate(Band srcBand, double contourInterval, double contourBase,
+#                int fixedLevelCount, int useNoData, double noDataValue,
+#                OGRLayerShadow * dstLayer, int idField, int elevField, 
+#                GDALProgressFunc callback=0, void * callback_data=None)
+driver = ogr.GetDriverByName('ESRI Shapefile')
+shp = driver.CreateDataSource('.')
+lyr = shp.CreateLayer(ofilepath, srs)
+f0_defn = ogr.FieldDefn('id', ogr.OFTInteger)
+f0 = lyr.CreateField(f0_defn)
+f1_defn = ogr.FieldDefn(varname, ogr.OFTReal)
+f1 = lyr.CreateField(f1_defn)
+gdal.ContourGenerate(band, 100.0, 0.5, [], 0, 0, lyr, f0, f1)
+
+# close datasets
+lyr = shp = None
+band = rast = None
