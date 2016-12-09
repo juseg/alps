f96a163a220e89b8fcc493e02ed8c3f4067f9524
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-12-09 16:10:52 +0100

    Add central uplift curve on uplift plot.

--- figures/uplift.py
+++ figures/uplift.py
@@ -2,18 +2,25 @@
 # coding: utf-8
 
 import util as ut
+import numpy as np
+
+# location of time series
+xts, yts = 600e3, 5170e3
 
 # initialize figure
-fig, ax, cax = ut.pl.subplots_cax_inset()
+fig, ax, cax, tsax = ut.pl.subplots_cax_ts_cut()
+
+
+# Map axes
+# --------
 
 # load extra data
-# FIXME: implement unit conversion (m to mm) in iceplotlib
 filepath = 'output/0.7.3/alps-wcnn-1km/epica3222cool0950+acyc1+esia5/extra.nc'
 nc = ut.io.load(filepath)
 x = nc.variables['x'][:]
 y = nc.variables['y'][:]
 age = -nc.variables['time'][:]/(1e3*365.0*24*60*60)
-dbdt = nc.variables['dbdt'][-1].T*1e3
+dbdt = nc.variables['dbdt'][:]*1e3
 
 # set levels and colors
 levs = [0.0, 0.4, 0.8, 1.2, 1.6, 2.0]
@@ -22,8 +29,14 @@ cols = cmap(range(len(levs)+1))
 
 # plot modern uplift
 im = nc.imshow('topg', ax, -1, vmin=0.0, vmax=3e3, cmap='Greys', zorder=-1)
-cs = ax.contourf(x, y, dbdt, thkth=-1, levels=levs, colors=cols,
+cs = ax.contourf(x, y, dbdt[-1].T, thkth=-1, levels=levs, colors=cols,
                  extend='both', alpha=0.75)
+
+# close nc file
+nc.close()
+
+# add location of time series
+ax.plot(xts, yts, 'o', c=ut.pl.palette['darkgreen'])
 
 # add cartopy vectors
 ut.pl.draw_natural_earth(ax)
@@ -32,8 +45,36 @@ ut.pl.draw_natural_earth(ax)
 cb = fig.colorbar(cs, cax)
 cb.set_label(r'modern uplift rate ($mm\,a^{-1}$)')
 
-# close nc file
+
+# Time series
+# -----------
+
+# compute central uplift rate
+its = np.argmin(abs(x-xts))
+jts = np.argmin(abs(y-yts))
+dbdt = dbdt[:, its, jts]
+
+# plot time series
+twax = tsax.twinx()
+twax.plot(age, dbdt, c=ut.pl.palette['darkgreen'])
+twax.plot(0.0, dbdt[-1], 'o', c=ut.pl.palette['darkgreen'], clip_on=False)
+twax.set_ylabel('uplift rate ($mm\,a^{-1}$)', color=ut.pl.palette['darkgreen'])
+twax.set_xlim(120.0, 0.0)
+twax.set_ylim(-15.0, 25.0)
+twax.locator_params(axis='y', nbins=6)
+twax.grid(axis='y')
+
+# load temperature signal
+nc = ut.io.load('input/dt/epica3222cool0950.nc')
+age = -nc.variables['time'][:]/1e3
+dt = nc.variables['delta_T'][:]
 nc.close()
+
+# plot temperature time series
+tsax.plot(age, dt, c='0.25')
+tsax.set_xlabel('model age (ka)')
+tsax.set_ylabel('temperature offset (K)', color='0.25')
+tsax.set_ylim(-12.5, 7.5)
 
 # save figure
 fig.savefig('uplift')
