f9b69f0d10adf7c74c3ba10ecbdeb3fbed56166f
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-12-16 19:35:45 +0100

    Replace uplift rate by topo diff on uplift plot.

--- figures/uplift.py
+++ figures/uplift.py
@@ -14,13 +14,27 @@ fig, ax, cax, tsax = ut.pl.subplots_cax_ts_cut()
 # Map axes
 # --------
 
+# load boot topography
+nc = ut.io.load('input/boot/alps-srtmsub+gou11simi-1km.nc')
+im = nc.imshow('topg', ax, vmin=0.0, vmax=3e3, cmap='Greys', zorder=-1)
+x = nc.variables['x'][:]
+y = nc.variables['y'][:]
+its = np.argmin(abs(x-xts))
+jts = np.argmin(abs(y-yts))
+boot = nc.variables['topg'][its, jts]
+nc.close()
+
 # load extra data
 filepath = 'output/0.7.3/alps-wcnn-1km/epica3222cool0950+acyc1+esia5/extra.nc'
 nc = ut.io.load(filepath)
 x = nc.variables['x'][:]
 y = nc.variables['y'][:]
+its = np.argmin(abs(x-xts))
+jts = np.argmin(abs(y-yts))
 age = -nc.variables['time'][:]/(1e3*365.0*24*60*60)
-dbdt = nc.variables['dbdt'][:]*1e3
+dbdt = nc.variables['dbdt'][-1]*1e3
+topg = nc.variables['topg'][:, its, jts]
+nc.close()
 
 # set levels and colors
 levs = [0.0, 0.4, 0.8, 1.2, 1.6, 2.0]
@@ -28,12 +42,8 @@ cmap = ut.pl.get_cmap('Greens', len(levs)+1)
 cols = cmap(range(len(levs)+1))
 
 # plot modern uplift
-im = nc.imshow('topg', ax, -1, vmin=0.0, vmax=3e3, cmap='Greys', zorder=-1)
-cs = ax.contourf(x, y, dbdt[-1].T, thkth=-1, levels=levs, colors=cols,
+cs = ax.contourf(x, y, dbdt.T, thkth=-1, levels=levs, colors=cols,
                  extend='both', alpha=0.75)
-
-# close nc file
-nc.close()
 
 # add location of time series
 ax.plot(xts, yts, 'o', c=ut.pl.palette['darkgreen'])
@@ -49,18 +59,14 @@ cb.set_label(r'modern uplift rate ($mm\,a^{-1}$)')
 # Time series
 # -----------
 
-# compute central uplift rate
-its = np.argmin(abs(x-xts))
-jts = np.argmin(abs(y-yts))
-dbdt = dbdt[:, its, jts]
-
 # plot time series
+diff = topg - boot
 twax = tsax.twinx()
-twax.plot(age, dbdt, c=ut.pl.palette['darkgreen'])
-twax.plot(0.0, dbdt[-1], 'o', c=ut.pl.palette['darkgreen'], clip_on=False)
-twax.set_ylabel('uplift rate ($mm\,a^{-1}$)', color=ut.pl.palette['darkgreen'])
+twax.plot(age, diff, c=ut.pl.palette['darkgreen'])
+twax.plot(0.0, diff[-1], 'o', c=ut.pl.palette['darkgreen'], clip_on=False)
+twax.set_ylabel('uplift (m)', color=ut.pl.palette['darkgreen'])
 twax.set_xlim(120.0, 0.0)
-twax.set_ylim(-15.0, 25.0)
+twax.set_ylim(-150.0, 250.0)
 twax.locator_params(axis='y', nbins=6)
 
 # save figure
