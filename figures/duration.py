37f83f69569942e808f3ed140012a7b0197ec346
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2016-12-16 19:24:37 +0100

    Add glaciated area timeseries on duration plot.

--- figures/duration.py
+++ figures/duration.py
@@ -2,15 +2,21 @@
 # coding: utf-8
 
 import util as ut
+import numpy as np
 
 # initialize figure
-fig, ax, cax = ut.pl.subplots_cax_inset()
+fig, ax, cax, tsax = ut.pl.subplots_cax_ts_cut()
+
+
+# Map axes
+# --------
 
 # load extra data
 filepath = 'output/0.7.3/alps-wcnn-1km/epica3222cool0950+acyc1+esia5/extra.nc'
 nc = ut.io.load(filepath)
 x = nc.variables['x'][:]
 y = nc.variables['y'][:]
+age = -nc.variables['time'][:]/(1e3*365.0*24*60*60)
 thk = nc.variables['thk'][:]
 
 # compute footprint
@@ -33,10 +39,28 @@ nc.close()
 
 # add cartopy vectors
 ut.pl.draw_natural_earth(ax)
+ut.pl.draw_lgm_outline(ax)
 
 # add colorbar
 cb = fig.colorbar(cs, cax)
 cb.set_label(r'duration of glaciation (ka)')
+
+# Time series
+# -----------
+
+# compute glaciated areas in 1e3 km2
+dx = x[1] - x[0]
+dy = y[1] - y[0]
+area = (thk >= 1.0).sum(axis=(1, 2))*dx*dy*1e-9
+
+# plot time series
+c = ut.pl.palette['darkblue']
+twax = tsax.twinx()
+twax.plot(age, area, c=c)
+twax.set_ylabel(r'glaciated area ($10^3\,km^2$)', color=c)
+twax.set_xlim(120.0, 0.0)
+twax.set_ylim(-25.0, 175.0)
+twax.locator_params(axis='y', nbins=6)
 
 # save figure
 fig.savefig('duration')
