273830bb5d3c4d7b64e99b1bc14e14281e8867af
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-06-01 16:54:55 +0200

    Use 2km runs for footprints and timeseries.

--- figures/util/__init__.py
+++ figures/util/__init__.py
@@ -13,6 +13,7 @@ import pl
 alpcyc_records = ['GRIP']*2 + ['EPICA']*2 + ['MD01-2444']*2
 alpcyc_configs = ['alpcyc4%s+till1545' % s for s in ['', '+pp']]*3
 alpcyc_offsets = [7.0, 9.0, 8.8, 11.3, 7.3, 9.7]  # total area scaling
+alpcyc_offsets = [7.0, 9.0, 8.8, 11.4, 7.3, 9.8]  # total area scaling 2km
 alpcyc_offsets = [8.3, 10.5, 9.8, 12.3, 8.2, 10.7]  # Rhine area scaling
 alpcyc_colours = [pl.palette[tone+hue] for hue in ['blue', 'red', 'green']
                                           for tone in ['dark', 'light']]
