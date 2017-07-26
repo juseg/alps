4e2d0316391f88c1c5caee8ab51e8df308992779
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-07-26 10:07:36 +0200

    Use void circles in trimline scatter.
    For points with a temperate layer of less than 1 m.

--- figures/alpcyc_hr_trimlines.py
+++ figures/alpcyc_hr_trimlines.py
@@ -29,18 +29,21 @@ nc = ut.io.load(filepath)
 x = nc.variables['x'][:]
 y = nc.variables['y'][:]
 h = nc.variables['thk'][:].max(axis=0)
-#w = nc.variables['tempicethk_basal'][:].max(axis=0)
+w = nc.variables['tempicethk_basal'][:].max(axis=0)
 nc.close()
 
 # get model elevation at trimline locations
 i = np.argmin(abs(xt[:, None] - x), axis=1)
 j = np.argmin(abs(yt[:, None] - y), axis=1)
 hm = h[j, i]
-#wm = w[j, i]
+wm = w[j, i]
 
 # draw scatter plot
 ax.scatter(zt, zt+hm, c=ut.pl.palette['darkblue'], alpha=0.75)
-#ax.scatter(zt, zt+wm, c=ut.pl.palette['darkred'], alpha=0.75)
+ax.scatter(zt[wm>=1.0], (zt+wm)[wm>=1.0],
+           c=ut.pl.palette['darkred'], alpha=0.75)
+ax.scatter(zt[wm<1.0], (zt+wm)[wm<1.0],
+           facecolors='none', edgecolors=ut.pl.palette['darkred'], alpha=0.75)
 ax.set_xlabel('observed trimline elevation (m)')
 ax.set_ylabel('modelled surface elevation (m)', labelpad=2)
 
@@ -54,7 +57,7 @@ ax.plot(ztfit, zmfit, c=ut.pl.palette['darkblue'], zorder=0)
 ax.text(ztfit[-1], ztfit[-1]+50.0, '1:1', ha='right')
 
 # add equation and mean diff
-eqn = '$z_m = %.3f \cdot z_t %.3f$' % tuple(c)
+eqn = '$z_m = %.3f \cdot z_t + %.3f$' % tuple(c)
 diff = hm.mean()
 note = '%s\n\nmean difference: %.3f m' % (eqn, diff)
 ax.text(0.95, 0.05, note, ha='right', color=ut.pl.palette['darkblue'],
