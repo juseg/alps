c16289116b50e798cfdda1fa638aa683a53deb33
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-08-04 16:15:39 +0200

    Use thickness not elev on y-axis in trimline plot.

--- figures/alpcyc_hr_trimlines.py
+++ figures/alpcyc_hr_trimlines.py
@@ -35,30 +35,28 @@ nc.close()
 # get model elevation at trimline locations
 i = np.argmin(abs(xt[:, None] - x), axis=1)
 j = np.argmin(abs(yt[:, None] - y), axis=1)
-hm = h[j, i]
-wm = w[j, i]
+ht = h[j, i]
+wt = w[j, i]
 
 # draw scatter plot
-ax.scatter(zt, zt+hm, c=ut.pl.palette['darkblue'], alpha=0.75)
-ax.scatter(zt[wm>=1.0], (zt+wm)[wm>=1.0],
+ax.scatter(zt, ht, c=ut.pl.palette['darkblue'], alpha=0.75)
+ax.scatter(zt[wt>=1.0], wt[wt>=1.0],
            c=ut.pl.palette['darkred'], alpha=0.75)
-ax.scatter(zt[wm<1.0], (zt+wm)[wm<1.0],
+ax.scatter(zt[wt<1.0], wt[wt<1.0],
            facecolors='none', edgecolors=ut.pl.palette['darkred'], alpha=0.75)
-ax.set_xlabel('observed trimline elevation (m)')
-ax.set_ylabel('modelled surface elevation (m)', labelpad=2)
+ax.set_xlabel('observed trimline elevation $z_t$ (m)')
+ax.set_ylabel('modelled max ice thickness $h_t$ (m)', labelpad=2)
 
 # compute linear fit
-c = np.polyfit(zt, zt+hm, 1)
+c = np.polyfit(zt, ht, 1)
 p = np.poly1d(c)
 ztfit = np.array([2000, 3200])
-zmfit = p(ztfit)
-ax.plot(ztfit, ztfit, c='k')
-ax.plot(ztfit, zmfit, c=ut.pl.palette['darkblue'], zorder=0)
-ax.text(ztfit[-1], ztfit[-1]+50.0, '1:1', ha='right')
+htfit = p(ztfit)
+ax.plot(ztfit, htfit, c=ut.pl.palette['darkblue'], zorder=0)
 
 # add equation and mean diff
-eqn = '$z_m = %.3f \cdot z_t + %.3f$' % tuple(c)
-diff = hm.mean()
+eqn = '$h_t = %.3f \cdot z_t + %.3f$' % tuple(c)
+diff = ht.mean()
 note = '%s\n\nmean difference: %.3f m' % (eqn, diff)
 ax.text(0.95, 0.05, note, ha='right', color=ut.pl.palette['darkblue'],
         transform=ax.transAxes)
