8b47c599cb273a78efd7e6d8ab6f47a61cf2ed32
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-08-04 18:44:06 +0200

    Color trimline scatter plot by max thk age.

--- figures/alpcyc_hr_trimlines.py
+++ figures/alpcyc_hr_trimlines.py
@@ -8,6 +8,7 @@ import numpy as np
 figw, figh = 85.0, 60.0
 fig = ut.pl.figure(figsize=(figw/25.4, figh/25.4))
 ax = fig.add_axes([10.0/figw, 7.5/figh, 50.0/figw, 50.0/figh])
+cax = fig.add_axes([12.5/figw, 35.0/figh, 5.0/figw, 20.0/figh])
 hsax = fig.add_axes([62.5/figw, 7.5/figh, 20.0/figw, 50.0/figh], sharey=ax)
 
 
@@ -29,22 +30,39 @@ filepath = ut.alpcyc_bestrun + 'y???????-extra.nc'
 nc = ut.io.load(filepath)
 x = nc.variables['x'][:]
 y = nc.variables['y'][:]
-h = nc.variables['thk'][:].max(axis=0)
+age = -nc.variables['time'][:]/(1e3*365.0*24*60*60)
+thk = nc.variables['thk'][:]
 nc.close()
+
+# compute max thickness and its age
+lgmage = age[thk.argmax(axis=0)]
+maxthk = thk.max(axis=0)
 
 # get model elevation at trimline locations
 i = np.argmin(abs(xt[:, None] - x), axis=1)
 j = np.argmin(abs(yt[:, None] - y), axis=1)
-ht = h[j, i]
+ht = maxthk[j, i]
+at = lgmage[j, i]
 
 
 # Scatter axes
 # ------------
 
+# set contour levels and colors
+mpl = ut.pl.iplt.matplotlib
+levs = range(21, 28)
+cmap = ut.pl.get_cmap('Paired', 12)
+cols = cmap(range(12))[:len(levs)+1]
+cmap, norm = mpl.colors.from_levels_and_colors(levs, cols, extend='both')
+
 # draw scatter plot
-ax.scatter(zt, ht, c=ut.pl.palette['darkblue'], alpha=0.75)
+sc = ax.scatter(zt, ht, c=at, cmap=cmap, norm=norm, alpha=0.75)
 ax.set_xlabel('observed trimline elevation $z_t$ (m)')
 ax.set_ylabel('modelled max ice thickness $h_t$ (m)', labelpad=2)
+
+# add colorbar
+cb = fig.colorbar(sc, cax)
+cb.set_label(r'age (ka)')
 
 
 # Histogram axes
