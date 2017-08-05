07f2252578a87324c02f6822dbd6ec6ef9eff5ec
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-08-05 20:16:39 +0200

    Implement interpolation along a (simple) profile.

--- figures/alpcyc_hr_profiles.py
+++ figures/alpcyc_hr_profiles.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python2
-
 # coding: utf-8
 
 import util as ut
 import numpy as np
+import scipy as sp
 
 # parameters
-xp = 540e3
-yd = 5135e3
+xp = np.array([480e3, 510e3, 540e3, 540e3, 550e3, 510e3, 480e3])
+yp = np.array([5170e3, 5180e3, 5190e3, 5230e3, 5260e3, 5290e3, 5290e3])
 c = ut.pl.palette['darkblue']
 
 # initialize figure
 figw, figh = 170.0, 60.0
 fig = ut.pl.figure(figsize=(figw/25.4, figh/25.4))
-ax1 = fig.add_axes([2.5/figw, 7.5/figh, 35/figw, 50/figh], projection=ut.pl.utm)
-ax2 = fig.add_axes([40.0/figw, 7.5/figh, 120/figw, 50/figh])
-ut.pl.add_subfig_label('(a)', ax=ax1)
-ut.pl.add_subfig_label('(b)', ax=ax2)
+ax = fig.add_axes([2.5/figw, 7.5/figh, 35/figw, 50/figh], projection=ut.pl.utm)
+tsax = fig.add_axes([40.0/figw, 7.5/figh, 120/figw, 50/figh])
+
+# prepare map axes
+ax.set_rasterization_zorder(2.5)
+ax.set_extent(ut.pl.regions['rhine'], crs=ax.projection)
+
+# add subfigure labels
+ut.pl.add_subfig_label('(a)', ax=ax)
+ut.pl.add_subfig_label('(b)', ax=tsax)
 
 
 # Map axes
 # --------
-ax = ax1
-
-# set extent around Rhine catchment
-ax.set_extent(ut.pl.regions['rhine'], crs=ax.projection)
 
 # draw boot topography
 nc = ut.io.load('input/boot/alps-srtm+thk+gou11simi-1km.nc')
@@ -37,25 +39,29 @@ ut.pl.draw_lgm_outline(ax)
 ut.pl.draw_footprint(ax)
 
 # add profile line
-ax.axvline(xp, c=c, ls='--', dashes=(2, 2))
-ax.plot(xp, yd, c=c, marker='o')
+ax.plot(xp, yp, c=c, ls='--', dashes=(2, 2))
+ax.plot(xp[0], yp[0], c=c, marker='o')
 
 
 # Time series
 # -----------
-ax = ax2
 
 # load extra data
 filepath = ut.alpcyc_bestrun + 'y???????-extra.nc'
 nc = ut.io.load(filepath)
 x = nc.variables['x'][:]
 y = nc.variables['y'][:]
+t = nc.variables['time'][9::10]/(365.0*24*60*60)
+h = nc.variables['thk'][9::10]
+nc.close()
 
 # extract space-time slice
-i = np.argmin(np.abs(x-xp))
-age = -nc.variables['time'][:]/(1e3*365*24*60*60)
-thk = nc.variables['thk'][:, :, i]
-nc.close()
+xi = t[:, None], yp[None, :], xp[None, :]  # coords to sample at
+hp = sp.interpolate.interpn((t, y, x), h, xi, method='linear')
+
+# compute distance along profile
+dp = (((xp[1:]-xp[:-1])**2+(yp[1:]-yp[:-1])**2)**0.5).cumsum()
+dp = np.insert(dp, 0, 0.0)
 
 # plot isotope stages
 ut.pl.plot_mis(ax, y=0.925)
@@ -63,16 +69,16 @@ ut.pl.plot_mis(ax, y=0.925)
 # plot envelope
 levs = [1.0, 5e3]
 cols = [c]
-cs = ax.contourf(age, (y-yd)/1e3, thk.T, levels=levs, colors=cols)
+cs = tsax.contourf(-t/1e3, dp/1e3, hp.T, levels=levs, colors=cols)
 
 # set axes properties
-ax.set_xlim(120.0, 0.0)
-ax.set_ylim(-60.0, 190.0)
-ax.set_xlabel('model age (ka)')
-ax.set_ylabel('distance from divide (km)')
-ax.yaxis.set_label_position("right")
-ax.yaxis.tick_right()
-ax.grid(axis='y')
+tsax.set_xlim(120.0, 0.0)
+#tsax.set_ylim(-60.0, 190.0)
+tsax.set_xlabel('model age (ka)')
+tsax.set_ylabel('distance along profile (km)')
+tsax.yaxis.set_label_position("right")
+tsax.yaxis.tick_right()
+tsax.grid(axis='y')
 
 # save
 fig.savefig('alpcyc_hr_profiles')
