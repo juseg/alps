e34e616c7c26c952c195565652ddfedb7911c690
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-08-15 16:04:42 +0200

    Add envelope and LGM age in ALPCYC postprocessing.

--- data/alpcyc_gtif_shp.py
+++ data/alpcyc_gtif_shp.py
@@ -13,12 +13,15 @@ x = nc.variables['x'][:]
 y = nc.variables['y'][:]
 age = -nc.variables['time'][:]/(365.0*24*60*60)
 thk = nc.variables['thk'][:]
+srf = nc.variables['usurf'][:]
 nc.close()
 
 # compute aggregated variables
 dt = age[0] - age[1]
 duration = (thk >= 1.0).sum(axis=0)*dt
+envelope = srf.max(axis=0)
 footprint = (duration > 0.0)
+lgmtiming = age[srf.argmax(axis=0)]
 
 # compute deglaciation age
 nlastfree = (thk >= 1.0)[::-1].argmax(axis=0)  # number of last free timesteps
@@ -31,4 +34,6 @@ prefix = '-'.join(map(os.path.basename, os.path.split(ut.alpcyc_bestrun)))
 prefix = os.path.join('processed', prefix+'-')
 ut.make_gtif_shp(x, y, deglacage, prefix+'deglacage', range(0, 30001, 1000))
 ut.make_gtif_shp(x, y, duration, prefix+'duration', range(50, 120000, 10000))
+ut.make_gtif_shp(x, y, envelope, prefix+'envelope', range(0, 4001, 200))
 ut.make_gtif_shp(x, y, footprint, prefix+'footprint', [0.5], dtype='byte')
+ut.make_gtif_shp(x, y, lgmtiming, prefix+'lgmtiming', range(21000, 27001, 1000))
