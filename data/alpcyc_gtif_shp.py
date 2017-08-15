904c8366e2afeb849188c4012bb2fa4cc7e279b4
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-08-15 15:31:31 +0200

    Clean up ALPCYC postprocessing.
    Processing time on vierzack03 3m31s -> 3m25s.

--- data/alpcyc_gtif_shp.py
+++ data/alpcyc_gtif_shp.py
@@ -3,16 +3,10 @@
 
 """Prepare ALPCYC aggregated geotiffs and shapefiles."""
 
-import os
+import os.path
 import util as ut
-import numpy as np
-
-# output files prefix
-prefix = '-'.join(map(os.path.basename, os.path.split(ut.alpcyc_bestrun)))
-prefix = os.path.join('processed', prefix+'-')
 
 # read extra output
-print 'reading extra output...'
 filename = ut.alpcyc_bestrun + '/y???????-extra.nc'
 nc = ut.load(filename)
 x = nc.variables['x'][:]
@@ -21,53 +15,20 @@ age = -nc.variables['time'][:]/(365.0*24*60*60)
 thk = nc.variables['thk'][:]
 nc.close()
 
+# compute aggregated variables
+dt = age[0] - age[1]
+duration = (thk >= 1.0).sum(axis=0)*dt
+footprint = (duration > 0.0)
+
 # compute deglaciation age
-print 'computing deglaciation age...'
-icy = (thk >= 1.0)
-nlastfree = icy[::-1].argmax(axis=0)  # number of last free timesteps
-deglacage = (age[-nlastfree-1]+age[-nlastfree]) / 2
+nlastfree = (thk >= 1.0)[::-1].argmax(axis=0)  # number of last free timesteps
+deglacage = age[-nlastfree] + dt/2
+deglacage[duration==120e3] = age[-1]
+deglacage[duration==0.0] = age[0]
 
-# fill in always and never icy cases
-nevericy = (nlastfree == 0)*(icy[-1] == 0)
-alwaysicy = (nlastfree == 0)*(icy[-1] == 1)
-deglacage[alwaysicy] = age[-1]
-deglacage[nevericy] = age[0]
-
-# file paths
-varname = 'deglacage'
-ofilepath = prefix + varname
-
-# make geotiff
-levs = np.arange(0e3, 30e3+1, 1e3)
-ut.make_gtif_shp(x, y, deglacage, ofilepath, levs, dtype='float32', epsg=32632,
-                 varname=varname)
-
-# ----
-
-# compute duration of ice cover
-print 'computing duration of ice cover...'
-duration = (thk >= 1.0).sum(axis=0)*100.0
-
-# file paths
-varname = 'duration'
-ofilepath = prefix + varname
-
-# make geotiff
-levs = np.arange(50.0, 120e3, 10e3)
-ut.make_gtif_shp(x, y, duration, ofilepath, levs, dtype='float32', epsg=32632,
-                 varname=varname)
-
-# ----
-
-# compute footprint
-print 'computing footprint...'
-footprint = 1 - (thk < 1.0).prod(axis=0)
-
-# file paths
-varname = 'footprint'
-ofilepath = prefix + varname
-
-# make geotiff
-levs = [0.5]
-ut.make_gtif_shp(x, y, footprint, ofilepath, levs, dtype='byte', epsg=32632,
-                 varname=varname)
+# export geotiffs and shapefiles
+prefix = '-'.join(map(os.path.basename, os.path.split(ut.alpcyc_bestrun)))
+prefix = os.path.join('processed', prefix+'-')
+ut.make_gtif_shp(x, y, deglacage, prefix+'deglacage', range(0, 30001, 1000))
+ut.make_gtif_shp(x, y, duration, prefix+'duration', range(50, 120000, 10000))
+ut.make_gtif_shp(x, y, footprint, prefix+'footprint', [0.5], dtype='byte')
