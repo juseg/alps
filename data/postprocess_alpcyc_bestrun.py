4f9bc71dc0c71e0f36559b9a1eb7b671c274eaf3
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-08-17 14:02:27 +0200

    Use dask serial scheduler when threaded one fails.
    Tried updating to latest netCDF library, and even migrated from pip to conda.
    This seem to be the only way to work around the posixio error...

--- data/postprocess_alpcyc_bestrun.py
+++ data/postprocess_alpcyc_bestrun.py
@@ -6,6 +6,7 @@
 import os.path
 import util as ut
 import numpy as np
+import dask
 import dask.array as da
 
 # chunk size
@@ -29,12 +30,13 @@ dt = age[0] - age[1]
 # create shortcut for ice mask
 icy = (thk >= 1.0)
 
-# pseudo mask certain variables (this is a workaround, apparently dask fails
-# to perform multiplication on two netCDF variables, posixio assertion failed)
-cba[~icy] = 0.0
-tpa[~icy] = -99.0
+# apply pseudo-masks
+cba = icy*cba
+tpa = icy*tpa
 
-# compute aggregated variables
+# compute aggregated variables (apparently dask fails to perform parallel
+# multiplication on two netCDF variables with posixio assertion failed,
+# therefore we use the serial scheduler dask.get)
 print "computing deglaciation age..."
 deglacage = age[-icy[::-1].argmax(axis=0).compute()]
 print "computing ice cover duration..."
@@ -42,8 +44,8 @@ duration = icy.sum(axis=0).compute()*dt
 print "computing surface envelope..."
 envelope = srf.max(axis=0).compute()
 print "computing total erosion..."
-ero = 2.7e-7*cba**2.02  # (Herman et al, 2015)
-erosion = ero.sum(axis=0).compute()*dt
+ero = 2.7e-7*icy*cba**2.02  # m/a (Herman et al, 2015)
+erosion = ero.sum(axis=0).compute(get=dask.get)*dt  # m
 print "computing number of glaciations..."
 nadvances = (icy[0] + (icy[1:]-icy[:-1]).sum(axis=0) + icy[-1]).compute()/2
 print "computing last glacial maximum timing..."
@@ -51,11 +53,11 @@ lgmtiming = age[srf.argmax(axis=0).compute()]
 print "computing maximum ice thickness..."
 maxicethk = thk.max(axis=0).compute()
 print "computing total basal sliding..."
-totalslip = cba.sum(axis=0).compute()*dt
+totalslip = cba.sum(axis=0).compute(get=dask.get)*dt
 print "computing MIS2 warm-based ice cover..."
 mis2 = (age < 29.0) * (age >= 14.0)
 mis2print = ~icy[mis2].prod(axis=0).compute()
-warmbased = (tpa[mis2] >= -1e-3).sum(axis=0).compute()*dt
+warmbased = (tpa[mis2] >= -1e-3).sum(axis=0).compute(get=dask.get)*dt
 
 # compute derived variables
 print "computing footprint..."
@@ -64,9 +66,9 @@ modernice = (duration == 120.0)
 
 # compute timeseries
 print "computing sliding flux..."
-slidingflux = cba.sum(axis=(1, 2)).compute()*dx*dy*1e-9  # km3/a
+slidingflux = cba.sum(axis=(1, 2)).compute(get=dask.get)*dx*dy*1e-9  # km3/a
 print "computing erosion rate..."
-erosionrate = ero.sum(axis=(1, 2)).compute()*dx*dy*1e-9  # m/a
+erosionrate = ero.sum(axis=(1, 2)).compute(get=dask.get)*dx*dy*1e-9  # km3/a
 
 # close extra output
 nc.close()
