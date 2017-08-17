9cc286c19f921cefb51bc64da7e0713a3b3a8461
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2017-08-17 16:55:26 +0200

    Merge cpugrid and locmap figures.

--- figures/alpcyc_hr_locmap.py
+++ figures/alpcyc_hr_locmap.py
@@ -7,19 +7,15 @@ import numpy as np
 # initialize figure
 fig, ax, cax = ut.pl.subplots_cax_inset()
 
-# boot topography
-nc = ut.io.load('input/boot/alps-srtm+thk+gou11simi-1km.nc')
-im = nc.imshow('topg', ax=ax, vmin=0e3, vmax=3e3, cmap='Greys', zorder=-1)
-nc.close()
+# add map elements
+im = ut.pl.draw_boot_topo(ax)
+ut.pl.draw_natural_earth(ax, graticules=False)
+ut.pl.draw_lgm_outline(ax)
+ut.pl.draw_cpu_grid(ax)
 
 # add colorbar
 cb = fig.colorbar(im, cax, extend='both', ticks=range(0, 3001, 1000))
 cb.set_label(r'bedrock topography (m)')
-
-# add vectors
-ut.pl.draw_major_cities(ax)
-ut.pl.draw_natural_earth(ax)
-ut.pl.draw_lgm_outline(ax)
 
 # save figure
 fig.savefig('alpcyc_hr_locmap')
