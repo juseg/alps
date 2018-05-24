eb560b0ce0f3b1f1fa42e2a9341b5448e04a45e4
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-05-24 16:10:00 +0900

    Add alpflo 500 m timestamps figure.

---
+++ figures/alpflo_timestamps.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import util as ut
+import glob
+import os.path
+import numpy as np
+
+# initialize figure
+fig, ax = ut.pl.subplots_ts()
+
+# open extra files
+globpath = os.path.join(os.environ['HOME'], 'pism', ut.alpflo_bestrun,
+                        'y???????-extra.nc')
+filelist = glob.glob(globpath)
+filelist.sort()
+
+# concatenate timestamps
+age = np.array([])
+stamp = np.array([])
+for filepath in filelist:
+    nc = ut.io.load(filepath)
+    age = np.append(age, -nc.variables['time'][:]/(365.0*24*60*60*1000))
+    stamp = np.append(stamp, nc.variables['timestamp'][:])
+    nc.close()
+
+# calculate computing speed and node hours
+speed = stamp - np.insert(stamp[:-1], 0, 0.0)
+speed = np.where(speed>0, speed, stamp)
+cumnh = speed.cumsum()/24.0
+
+# plot
+c = ut.pl.palette['darkblue']
+ax.plot(age, cumnh, color=c)
+
+# mark scalability test
+#testage = 120.0 - 57.5
+#ax.plot(testage, cumnh[age == testage], color=c, marker='o')
+#ax.text(testage, cumnh[age == testage]+0.5, 'scalability test  ', color=c, ha='right')
+
+# set axes properies
+#ax.set_xlim(120.0, 0.0)
+ax.invert_xaxis()
+ax.set_xlabel('model time (ka)')
+ax.set_ylabel('computing time (days)')
+ax.grid()
+
+# save
+ut.pl.savefig()
