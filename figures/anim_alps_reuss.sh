a2b4046549cc3fa6933503c898cc92326c8b8eb2
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-07-04 03:59:49 +0200

    Embed jumpers in animations using ffmpeg.

--- figures/anim_alps_reuss.sh
+++ figures/anim_alps_reuss.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-name=$(basename "${0%.sh}")
+# animation name
+name=$(basename "${0%.sh}")${1+_$1}
 
-ffmpeg -pattern_type glob -i "$HOME/anim/$name/??????.png" -r 25 \
-    -pix_fmt yuv420p -c:v libx264 $name.mp4 -y
+# prepare jumpers
+for f in $name anim_alps_{sources,reality,license}${1+_$1}
+do
+    [ -f $f.png ] || inkscape --export-dpi=254 --export-png=$f.png $f.svg
+done
+
+# assemble video
+ffmpeg -loop 1 -t 10 -i $name.png \
+       -pattern_type glob -i "$HOME/anim/$name/??????.png" \
+       -loop 1 -t 10 -i anim_alps_sources${1+_$1}.png \
+       -loop 1 -t 05 -i anim_alps_reality${1+_$1}.png \
+       -loop 1 -t 05 -i anim_alps_license${1+_$1}.png \
+       -filter_complex \
+           "[0:v]fade=t=in:st=0:d=1,fade=t=out:st=009:d=1[v0]; \
+            [1:v]fade=t=in:st=0:d=1,fade=t=out:st=119:d=1[v1]; \
+            [2:v]fade=t=in:st=0:d=1,fade=t=out:st=009:d=1[v2]; \
+            [3:v]fade=t=in:st=0:d=1,fade=t=out:st=004:d=1[v3]; \
+            [4:v]fade=t=in:st=0:d=1,fade=t=out:st=004:d=1[v4]; \
+            [v0][v1][v2][v3][v4]concat=n=5:v=1:a=0,format=yuv420p[v]" \
+       -map "[v]" -c:v libx264 -r 25 $name.mp4 -y
