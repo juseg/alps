2d15af53559fc2094101887965528999055b2aed
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-07-15 17:10:50 +0900

    Add 9k animation tiling script.

---
+++ figures/anim_alps_9k.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# animation name
+name=$(basename "${0%.sh}")
+orig="$HOME/anim/$name"  # folder containing animation frames
+dest="$name.tms"  # destination folder for tiles
+
+# find gdal2tiles command
+gdal2tiles="$(command -v gdal2tiles.py || command -v gdal2tiles2.py)"
+
+# loop on frames
+for year in {005..120..5}000
+do
+
+    # locally link current frame
+    ln -sf $orig/$year.png $name.png
+
+    # prepare tiles
+    [ -d $dest/$year ] || $gdal2tiles -r antialias -s "EPSG:32632" \
+        -w none -z 6-10 $name.png $dest/$year
+
+done
