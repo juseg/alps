6cd75a56417d6161e97cfb304a86297c30f97071
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-08-24 16:05:28 +0900

    Dub this continuous variables, better filenames.

--- data/postprocess-alpcyc-cts.sh
+++ data/postprocess-alpcyc-cts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Prepare ALPCYC 1ka snapshots and scalar timeseries.
-#
+# Prepare ALPCYC continuous variables
+# -----------------------------------
+
 # I find NCO syntax a bit clumsy, and preserving history far from intuitive.
 # It may actually be easier to use python and xarray.
 
@@ -37,12 +38,12 @@ do
     efile="$HOME/pism/output/e9d2d1f/alps-wcnn-$res/${rec}3222cool$dt+$conf"
     blink="alpcyc.$res.boot.nc"                 # link to boot file
     elink="alpcyc.$res.${rec:0:4}.$pp.extra"    # link to extra file
-    ofile="alpcyc.$res.${rec:0:4}.$pp.1ka.nc"   # snapshots output file
-    sfile="alpcyc.$res.${rec:0:4}.$pp.sts.nc"   # timeseries output file
-    tfile="alpcyc.$res.${rec:0:4}.$pp.tmp.nc"   # multipurpose tmp file
+    pexfile="alpcyc.$res.${rec:0:4}.$pp.ex.1ka.nc"  # processed extra file
+    ptsfile="alpcyc.$res.${rec:0:4}.$pp.ts.10a.nc"  # timeseries output file
+    tmpfile="alpcyc.$res.${rec:0:4}.$pp.tmp.nc"     # multipurpose tmp file
 
     # message
-    echo "preparing $ofile..."
+    echo "preparing $pexfile..."
 
     # link output files (newer nco forbids + in file names)
     # FIXME: remove all + in pism folders on all computers
@@ -50,43 +51,44 @@ do
     ln -fs $bfile $blink
 
     # concatenate output files and copy history from last file
-    ncrcat -O -d time,$stride -v ${evars// /,} $elink/*-extra.nc $ofile
-    ncks -A -h -x $elink/y0120000-extra.nc $ofile
-    ncrcat -O -d time,$stride -v ${evars// /,} $elink/*-ts.nc $sfile
-    ncks -A -h -x $elink/y0120000-ts.nc $sfile
+    ncrcat -O -d time,$stride -v ${evars// /,} $elink/*-extra.nc $pexfile
+    ncks -A -h -x $elink/y0120000-extra.nc $pexfile
+    ncrcat -O -d time,$stride -v ${evars// /,} $elink/*-ts.nc $ptsfile
+    ncks -A -h -x $elink/y0120000-ts.nc $ptsfile
 
     # apply mask and add fill value
     ncap2 -A -s "where(thk<1.0){$(for v in $mvars; do echo -n "$v=-2e9;"; done)};\
-    $(for v in $mvars; do echo -n "$v.set_miss(-2e9);"; done)" $ofile
+    $(for v in $mvars; do echo -n "$v.set_miss(-2e9);"; done)" $pexfile
 
     # extract boot topography, rename it, edit its attributes and append
-    ncks -O -v topg $blink $tfile
-    ncpdq -O -a y,x $tfile $tfile
-    ncrename -v topg,boot_topg $tfile
+    ncks -O -v topg $blink $tmpfile
+    ncpdq -O -a y,x $tmpfile $tmpfile
+    ncrename -v topg,boot_topg $tmpfile
     ncatted -h -a coordinates,boot_topg,o,c,"lat lon" \
                -a grid_mapping,boot_topg,o,c,"mapping" \
                -a long_name,boot_topg,o,c,"initial bedrock surface elevation" \
-               -a standard_name,boot_topg,o,c,"initial_bedrock_altitude" $tfile
-    ncks -A -v boot_topg $tfile $ofile
+               -a standard_name,boot_topg,o,c,"initial_bedrock_altitude" \
+               $tmpfile
+    ncks -A -v boot_topg $tmpfile $pexfile
 
     # add titles
     prefix="Alpine ice sheet glacial cycle simulations"
-    ncatted -h -a title,global,o,c,"$prefix millennial snapshots" $ofile
-    ncatted -h -a title,global,o,c,"$prefix scalar time series" $sfile
+    ncatted -h -a title,global,o,c,"$prefix spatial diagnostics" $pexfile
+    ncatted -h -a title,global,o,c,"$prefix scalar time series" $ptsfile
 
     # remove (mostly) duplicate history and add global attributes
     inst="ETH Zürich, Switzerland and Hokkaido University, Japan"
 
-    for f in $ofile $sfile
+    for f in $pexfile $ptsfile
     do
-        ncatted -h -a history_of_appended_files,global,d,, $ofile
-        ncatted -h -a author,global,o,c,"Julien Seguinot" $ofile
-        ncatted -h -a institution,global,o,c,"$inst" $ofile
+        ncatted -h -a history_of_appended_files,global,d,, $f
+        ncatted -h -a author,global,o,c,"Julien Seguinot" $f
+        ncatted -h -a institution,global,o,c,"$inst" $f
     done
 
     # compress with shuffling and remove links
-    nccopy -sd1 $ofile $tfile && mv $tfile $ofile
-    nccopy -sd1 $sfile $tfile && mv $tfile $sfile
+    nccopy -sd1 $pexfile $tmpfile && mv $tmpfile $pexfile
+    nccopy -sd1 $ptsfile $tmpfile && mv $tmpfile $ptsfile
     rm $blink $elink
 
 done
