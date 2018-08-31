b4b17bf04bd3c20900681d8cd0f62f667c6e27cb
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-08-31 16:02:07 +0900

    Use with statements in alpcyc agg postprocessing.

--- data/postprocess-alpcyc-agg.py
+++ data/postprocess-alpcyc-agg.py
@@ -52,101 +52,116 @@ for i in range(7):
     conf = 'alpcyc4' + ('+pp' if dp == 'pp' else '')
 
     # input and output file paths
-    rname = 'alps-wcnn-{}/{}3222cool{:04.0f}+{}'.format(res, rec, 100*dt, conf)
-    ipath = os.environ['HOME'] + '/pism/output/e9d2d1f/' + rname
+    pism_dir = os.path.join(os.environ['HOME'], 'pism')
+    dt_file = rec + '3222cool{:04.0f}'.format(100*dt)
+    run_dir = os.path.join(pism_dir, 'output', 'e9d2d1f', 'alps-wcnn-'+res,
+                           dt_file+'+alpcyc4'+('+pp' if dp == 'pp' else ''))
+    ex_file = run_dir+'/y???????-extra.nc'
+    ts_file = run_dir+'/y???????-ts.nc'
     ofile = 'processed/alpcyc.{}.{}.{}.agg.nc'.format(res, rec[:4], dp)
 
     # Load model output
     # -----------------
 
     # load output data
-    print "loading " + rname + "..."
-    ts = xr.open_mfdataset(ipath+'/y???????-ts.nc', decode_times=False)
-    ex = xr.open_mfdataset(ipath+'/y???????-extra.nc', decode_times=False,
-                           chunks={'time': 50}, data_vars='minimal')
+    print "loading " + run_dir + "..."
+    with xr.open_mfdataset(ex_file, concat_dim='time',
+                           chunks=dict(time=10), decode_cf=False,
+                           decode_times=False, data_vars='minimal') as ex:
 
-    # get global attributes from last file (xarray issue #2382)
-    last = xr.open_dataset(ipath+'/y0120000-extra.nc', decode_times=False)
-    ex.attrs = last.attrs
-    last.close()
+        # get global attributes from last file (xarray issue #2382)
+        with xr.open_dataset(ex_file.replace('???????', '0120000'),
+                             decode_times=False) as ds:
+            ex.attrs = ds.attrs
+            ds.close()
 
-    # register proxy variables and extract time step
-    ex['icy'] = (ex.thk >= 1.0).assign_attrs(units='')
-    ex['age'] = (-ex['time']/365/24/60/60).assign_attrs(units='years')
-    dt = ex['age'][0] - ex['age'][1]
+        # register proxy variables and extract time step
+        ex['icy'] = (ex.thk >= 1.0).assign_attrs(units='')
+        ex['age'] = (-ex['time']/365/24/60/60).assign_attrs(units='years')
+        dt = ex['age'][0] - ex['age'][1]
 
-    # init postprocessed dataset with global attributes
-    pp = xr.Dataset(attrs=ex.attrs, coords=dict(lon=ex.lon, lat=ex.lat))
+        # init postprocessed dataset with global attributes
+        pp = xr.Dataset(attrs=ex.attrs, coords=dict(lon=ex.lon, lat=ex.lat))
 
-    # Compute aggregated variables
-    # ----------------------------
+        # Compute aggregated variables
+        # ----------------------------
 
-    # compute glacial cycle integrated variables
-    attrs = message(long_name='ice cover duration', units='years')
-    pp['covertime'] = ex.icy.sum(axis=0).assign_attrs(attrs)*dt
-    attrs = message(long_name='deglaciation age', units='years')
-    i = ex.icy[::-1].argmax(axis=0).compute()
-    pp['deglacage'] = ex.age[-i].where(i > 0)
-    attrs = message(long_name='ice cover footprint', units='')
-    pp['footprint'] = (pp.covertime > 0.0).assign_attrs(attrs)
+        # compute glacial cycle integrated variables
+        attrs = message(long_name='ice cover duration', units='years')
+        pp['covertime'] = ex.icy.sum(axis=0).assign_attrs(attrs)*dt
+        attrs = message(long_name='deglaciation age', units='years')
+        i = ex.icy[::-1].argmax(axis=0).compute()
+        pp['deglacage'] = ex.age[-i].where(i > 0).assign_attrs(attrs)
+        attrs = message(long_name='ice cover footprint', units='')
+        pp['footprint'] = (pp.covertime > 0.0).assign_attrs(attrs)
 
-    # compute index of max ice extent
-    a = 24556.0  # max of slvol and volume_glacierized (1km)
-    a = 24568.0  # max of area_glacierized (1km)
-    t = ts.time[ts.area_glacierized.argmax(dim='time').compute()]
-    i = abs(ex.time-t).argmin()  # could also use ex.sel()
-    age_attrs = dict(age=ex.age[i].data, age_units=ex.age.units,
-                     time=ex.time[i].data, time_units=ex.time.units)
+        # compute index of max ice extent
+        with xr.open_mfdataset(ts_file, decode_times=False) as ds:
+            time = ds.time[ds.area_glacierized.argmax(axis=0).data]
+        i = abs(ex.time-time).argmin()  # could also use ex.sel()
 
-    # compute max extent snapshot variables
-    prefix = 'maximum extent '
-    ice = ex.icy[i]
-    attrs = message(long_name=prefix+'ice thickness')
-    pp['maxextthk'] = ex.thk[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'pressure-adjusted basal temperature')
-    pp['maxextbtp'] = ex.temppabase[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'basal temperate ice layer thickness')
-    pp['maxextbtt'] = ex.tempicethk_basal[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'basal velocity x-component')
-    pp['maxextbvx'] = ex.uvelbase[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'basal velocity y-component')
-    pp['maxextbvy'] = ex.vvelbase[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'ice surface elevation')
-    pp['maxextsrf'] = ex.usurf[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'surface velocity x-component')
-    pp['maxextsvx'] = ex.uvelsurf[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'surface velocity y-component')
-    pp['maxextsvy'] = ex.vvelsurf[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'bedrock topography')
-    pp['maxexttpg'] = ex.topg[i].assign_attrs(attrs)
+        # compute max extent snapshot variables
+        prefix = 'maximum extent '
+        ice = ex.icy[i]
+        attrs = message(long_name=prefix+'ice thickness')
+        pp['maxextthk'] = ex.thk[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'pressure-adjusted basal temperature')
+        pp['maxextbtp'] = ex.temppabase[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'basal temperate ice layer thickness')
+        pp['maxextbtt'] = ex.tempicethk_basal[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'basal velocity x-component')
+        pp['maxextbvx'] = ex.uvelbase[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'basal velocity y-component')
+        pp['maxextbvy'] = ex.vvelbase[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'ice surface elevation')
+        pp['maxextsrf'] = ex.usurf[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'surface velocity x-component')
+        pp['maxextsvx'] = ex.uvelsurf[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'surface velocity y-component')
+        pp['maxextsvy'] = ex.vvelsurf[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'bedrock topography')
+        pp['maxexttpg'] = ex.topg[i].assign_attrs(attrs)
 
-    # compute max thickness transgressive variables
-    prefix = 'maximum thickness '
-    icy = pp.footprint
-    attrs = message(long_name=prefix+'age')
-    i = ex.thk.argmax(dim='time').compute()
-    pp['maxthkage'] = ex.age[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'pressure-adjusted basal temperature')
-    pp['maxthkbtp'] = ex.temppabase[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'basal temperate ice layer thickness')
-    pp['maxthkbtt'] = ex.tempicethk_basal[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'basal velocity norm')
-    pp['maxthkbvn'] = ex.velbase_mag[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name=prefix+'ice surface elevation')
-    pp['maxthksrf'] = ex.usurf[i].where(ice).assign_attrs(attrs)
-    attrs = message(long_name='maximum ice thickness')
-    pp['maxthkthk'] = ex.thk[i].where(ice).assign_attrs(attrs)
+        # add age attributes
+        age_attrs = dict(age=ex.age[i].data, age_units=ex.age.units,
+                         time=ex.time[i].data, time_units=ex.time.units)
+        for name, variable in pp.iteritems():
+            if name.startswith('maxext'):
+                variable.attrs.update(age_attrs)
 
-    # add global attributes
+        # compute max thickness transgressive variables
+        prefix = 'maximum thickness '
+        icy = pp.footprint
+        attrs = message(long_name=prefix+'age')
+        i = ex.thk.argmax(dim='time').compute()
+        pp['maxthkage'] = ex.age[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'pressure-adjusted basal temperature')
+        pp['maxthkbtp'] = ex.temppabase[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'basal temperate ice layer thickness')
+        pp['maxthkbtt'] = ex.tempicethk_basal[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'basal velocity norm')
+        pp['maxthkbvn'] = ex.velbase_mag[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name=prefix+'ice surface elevation')
+        pp['maxthksrf'] = ex.usurf[i].where(ice).assign_attrs(attrs)
+        attrs = message(long_name='maximum ice thickness')
+        pp['maxthkthk'] = ex.thk[i].where(ice).assign_attrs(attrs)
+
+        # copy grid mapping and pism config
+        pp['mapping'] = ex.mapping
+        pp['pism_config'] = ex.pism_config
+
+    # edit global attributes
     pp.attrs.update(globs)
     pp.attrs['subtitle'] = ('{} {} simulation '.format(res, rec.upper()) +
                             ('with ' if dp == 'pp' else 'without ') +
                             'precipitation reductions')
     pp.attrs['history'] = pp.attrs['command'] + pp.attrs['history']
 
-    # copy grid mapping and pism config
-    pp['mapping'] = ex.mapping
-    pp['pism_config'] = ex.pism_config
+    # remove outdated attributes
+    for name, variable in pp.iteritems():
+        variable.attrs.pop('coordinates', None)
+        variable.attrs.pop('pism_intent', None)
+
 
     # Export aggregated data
     # ----------------------
@@ -157,12 +172,7 @@ for i in range(7):
 
     # export to netcdf
     print "* exporting aggregated data..."
-    pp.drop('time')
-    pp.to_netcdf(ofile, mode='w',
-                 encoding={var: {'zlib': True, 'shuffle': True, 'complevel': 5}
-                           for var in pp.variables})
-
-    # close datasets
-    ex.close()
-    pp.close()
-    ts.close()
+    pp = pp.drop('time')
+    encoding = dict(zlib=True, shuffle=True, complevel=5)
+    encoding = {var: encoding for var in pp.variables}
+    pp.to_netcdf(ofile, mode='w', encoding=encoding)
