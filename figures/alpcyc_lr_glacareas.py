0917174cd7f4c7090b77468cc12ee4b1a4dc9eed
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-09-03 13:51:31 +0900

    Merge glaciated area plots and use xarray.
    Remove global area plot, keep only rhine lobe area plot. It is easy to
    reproduce the other figure by changing the region bounds.

--- figures/alpcyc_lr_glacareas.py
+++ figures/alpcyc_lr_glacareas.py
@@ -4,14 +4,32 @@
 import util as ut
 import numpy as np
 
-# LGM extent UTM 32
-# original   131,679.803 km2
-# holefilled 149,027.868 km2
-# fullfilled 150,102.315 km²
-target = 150.102315
+# LGM ice extent in UTM 32
+# * Entire Alps
+#  - E&G original data:     131.679803 10^3km2
+#  - selected holes filled: 149.027868
+#  - all the holes filled:  150.102315
+# * Rhine lobe 5 km
+#  - E&G original data:      9.30466349736 10^3 km2
+#  - selected holes filled:  9.69897045426
+#  - all the holes filled:   9.92007028636
+# * Rhine lobe 2 km
+#  - E&G original data:      9.66219615876
+#  - selected holes filled: 10.06855769962
+#  - all the holes filled:  10.29263599490
+target = 10.29263599490
 
 # initialize figure
 fig, ax = ut.pl.subplots_ts()
+
+# resolutions and styles
+resolutions = ['5km', '2km']
+linestyles = [':', '-']
+linewidths = [0.5, 1.0]
+markers = ['+', '*']
+
+# domain for masking
+w, e, s, n = ut.pl.regions['rhlobe']
 
 # for each record
 for i, rec in enumerate(ut.alpcyc_records):
@@ -23,32 +41,33 @@ for i, rec in enumerate(ut.alpcyc_records):
 
     # loop on offsets
     for dt in np.arange(6.0, 14.1, 0.1):
+        dtfile = rec.replace('-', '').lower() + '3222cool{:04.0f}'.format(100*dt)
 
         # try to find max area
         try:
 
             # load extra file
-            dtfile = '%s3222cool%04d' % (rec.replace('-', '').lower(),
-                                         round(dt*100))
-            nc = ut.io.load('output/e9d2d1f/alps-wcnn-2km/%s+%s/'
-                            'y???????-extra.nc' % (dtfile, conf))
-            x = nc.variables['x'][:]
-            y = nc.variables['y'][:]
-            thk = nc.variables['thk'][909:1059]  # 29 to 14 ka
-            nc.close()
+            with ut.io.load_mfoutput('output/e9d2d1f/alps-wcnn-2km/%s+%s/'
+                                     'y???????-extra.nc' % (dtfile, conf)) as ds:
 
-            # compute footprint area in 1e3 km2
-            dx = x[1] - x[0]
-            dy = y[1] - y[0]
-            footprint = 1 - (thk < 1.0).prod(axis=0)
-            a = footprint.sum()*dx*dy*1e-9
-    
+                # select space-time of interest
+                agemask = (14.0 < ds.age) & (ds.age < 29)
+                xmask = (w < ds.x) & (ds.x < e)
+                ymask = (s < ds.y) & (ds.y < n)
+                thk = ds.thk.sel(age=agemask, y=ymask, x=xmask)
+
+                # compute glaciated area in 1e3 km2
+                dx = ds.x[1] - ds.x[0]
+                dy = ds.y[1] - ds.y[0]
+                footprint = 1 - (thk < 1.0).prod(axis=0)
+                area = footprint.sum()*dx*dy*1e-9
+
             # append to lists
             offsets.append(dt)
-            fpareas.append(a)
+            fpareas.append(area)
 
         # else do nothing
-        except (RuntimeError, IndexError, ValueError):
+        except (IOError):
             pass
 
     # continue if no files found
@@ -58,15 +77,15 @@ for i, rec in enumerate(ut.alpcyc_records):
     # plot
     argmin = np.argmin(np.abs(np.array(fpareas)-target))
     ax.plot(offsets, fpareas, c=c, marker='+', label=label)
+    ax.axhline(target, lw=0.1, c='0.5')
     ax.plot(offsets[argmin], fpareas[argmin], c=c, marker='D')
     ax.axvline(offsets[argmin], lw=0.1, c=c)
     for dt, a in zip(offsets, fpareas):
         if a:
-            ax.text(dt+0.1, a, '%.0f' % a, color=c, fontsize=4,
+            ax.text(dt+0.1, a, '%.1f' % a, color=c, fontsize=4,
                     ha='left', va='center', clip_on=True)
 
 # set axes properties
-ax.axhline(target, lw=0.1, c='0.5')
 ax.legend(loc='lower left', ncol=3)
 ax.set_xlim(ax.get_xlim()[0], ax.get_xlim()[1]+0.1)
 ax.set_xlabel('temperature offset (K)')
