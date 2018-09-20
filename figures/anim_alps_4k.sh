040b2de25dc4299fdd47f66777fd8f55c3883e68
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-09-20 19:51:24 +0900

    Update animation bash script.

--- figures/anim_alps_4k.sh
+++ figures/anim_alps_4k.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash
 
 # parse command-line arguments
 crop="${1:-al}"  # al ch
@@ -6,9 +6,9 @@ over="${2:-ttag}"  # ttag tbar
 lang="${3:-en}"  # de en fr
 
 # prepare bumpers
-for f in anim_alps_bp_{4k_${crop},refs,disc,bysa}_$lang
+for f in anim_alps_bp_{4k_${crop/zo/al},refs,disc,bysa}_$lang
 do
-    [ -f $f.png ] || inkscape --export-dpi=508 --export-png=$f.png $f.svg
+    inkscape --export-dpi=508 --export-png=$f.png $f.svg
 done
 
 # color box and overlay position
@@ -18,31 +18,37 @@ case $over in
     *) echo "invalid overlay $over, exiting." ;;
 esac
 
+# assembling parametres
+imgs="??????.png"  # images to include, edit for quick tests
+fade=12  # number of frames for fade in and fade out effects
+hold=25  # number of frames to hold in the beginning and end
+secs=$((120+2*hold/25))  # duration of main scene in seconds
+
 # prepare filtergraph for main scene
 filt=""
 filt+="color=${box}[c];"                # create time info color box
-filt+="nullsrc=s=3840x2160:d=124[n];"   # create fixed duration stream
+filt+="nullsrc=s=3840x2160:d=$secs[n];" # create fixed duration stream
 filt+="[0][1]overlay[geog];"            # assemble geographic layer
 filt+="[c][2]overlay=shortest=1[info];" # assemble time info layer
 filt+="[geog][info]overlay=${pos}"      # overlay time info on maps
-filt+=",loop=50:1:0,[n]overlay"         # hold first and last frames
+filt+=",loop=$hold:1:0,[n]overlay"      # hold first and last frames
 
 # add title frame and bumpers
-filt+=",fade=in:st=0:d=1,fade=out:st=123:d=1[main];"  # main scene
-filt+="[3]fade=in:st=0:d=1,fade=out:st=7:d=1[head];"  # title frame
-filt+="[4]fade=in:st=0:d=1,fade=out:st=5:d=1[refs];"  # references
-filt+="[5]fade=in:st=0:d=1,fade=out:st=5:d=1[disc];"  # disclaimer
-filt+="[6]fade=in:st=0:d=1,fade=out:st=5:d=1[bysa];"  # license
+filt+=",fade=in:0:$fade,fade=out:$((secs*25-fade)):$fade[main];"  # main scene
+filt+="[3]fade=in:0:$fade,fade=out:$((4*25-fade)):$fade[head];"  # title frame
+filt+="[4]fade=in:0:$fade,fade=out:$((3*25-fade)):$fade[refs];"  # references
+filt+="[5]fade=in:0:$fade,fade=out:$((3*25-fade)):$fade[disc];"  # disclaimer
+filt+="[6]fade=in:0:$fade,fade=out:$((3*25-fade)):$fade[bysa];"  # license
 filt+="[head][main][refs][disc][bysa]concat=5" \
 
 # assemble video
 ffmpeg \
-    -pattern_type glob -i "$HOME/anim/anim_alps_4k_main_${crop}/??????.png" \
-                       -i "$HOME/anim/anim_alps_4k_city_${crop}/$lang.png" \
-    -pattern_type glob -i "$HOME/anim/anim_alps_4k_${over}_$lang/??????.png" \
-    -loop 1 -t 8 -i anim_alps_bp_4k_${crop}_$lang.png \
-    -loop 1 -t 6 -i anim_alps_bp_refs_$lang.png \
-    -loop 1 -t 6 -i anim_alps_bp_disc_$lang.png \
-    -loop 1 -t 6 -i anim_alps_bp_bysa_$lang.png \
+    -pattern_type glob -i "$HOME/anim/anim_alps_4k_main_${crop}/$imgs" \
+    -pattern_type glob -i "$HOME/anim/anim_alps_4k_city_${crop}_$lang/$imgs" \
+    -pattern_type glob -i "$HOME/anim/anim_alps_4k_${over}_$lang/$imgs" \
+    -loop 1 -t 4 -i anim_alps_bp_4k_${crop/zo/al}_$lang.png \
+    -loop 1 -t 3 -i anim_alps_bp_refs_$lang.png \
+    -loop 1 -t 3 -i anim_alps_bp_disc_$lang.png \
+    -loop 1 -t 3 -i anim_alps_bp_bysa_$lang.png \
     -filter_complex $filt -pix_fmt yuv420p -c:v libx264 -r 25 \
-    anim_alps_4k_${crop}_${over}_${lang}.mp4
+    $HOME/anim/anim_alps_4k_${crop}_${over}_${lang}.mp4
