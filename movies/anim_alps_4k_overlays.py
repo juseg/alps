4213b56e0aedad6f4b68bd45a4335de527d0e18b
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-03-08 16:09:21 +0900

    Add missing city of Lucerne on Lucerne animations.

--- movies/anim_alps_4k_overlays.py
+++ movies/anim_alps_4k_overlays.py
@@ -12,12 +12,12 @@ def overlay_city(t, crop='al', lang='en', t0=-120e3, t1=0e3):
     """Plot city overlay for given language."""
 
     # initialize figure
-    fig, ax = ut.axes_anim_dynamic(
-        crop, t, t0=t0, t1=t1, figsize=(192.0, 108.0))
+    fig, ax = ut.axes_anim_dynamic(crop=crop, t=t, t0=t0, t1=t1,
+                                   figsize=(192.0, 108.0))
 
     # draw map elements
-    ut.draw_major_cities(
-        ax=ax, exclude='Monaco', include='Sion', maxrank=6, lang=lang)
+    ut.draw_major_cities(ax=ax, exclude='Monaco', include='Sion', lang=lang,
+                         maxrank=(8 if crop=='lu' else 6))
 
     # return figure
     return fig
