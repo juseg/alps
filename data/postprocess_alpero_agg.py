441e290e145da983b790b3f70a8d12a7e1ba91bc
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-11-08 16:03:39 +0100

    More cleanup in alpero postprocessing script.

--- data/postprocess_alpero_agg.py
+++ data/postprocess_alpero_agg.py
@@ -10,11 +10,12 @@ import sys
 import datetime
 import xarray as xr
 
-# Global parameters
-# -----------------
-
-# add global attributes
-globs = dict(
+# global attributes
+PROC_RUNS = ['alpcyc4.2km.grip.0820', 'alpcyc4.2km.grip.1040.pp',
+             'alpcyc4.2km.epica.0970', 'alpcyc4.2km.epica.1220.pp',
+             'alpcyc4.2km.md012444.0800', 'alpcyc4.2km.md012444.1060.pp',
+             'alpcyc4.1km.epica.1220.pp']
+GLOB_ATTRS = dict(
     title='Alpine ice sheet glacial cycle erosion aggregated variables',
     author='Julien Seguinot',
     institution='ETH Zürich, Switzerland and Hokkaido University, Japan',
@@ -27,21 +28,27 @@ globs = dict(
 # --------------------
 
 
-def postprocess(ipath, ofile, subtitle):
+def postprocess(run_path):
+    """Postprocess extra dataset for one run."""
+
+    # output file and subtitle
+    res, rec, *other = os.path.basename(run_path).split('.')
+    out_file = 'processed/alpero.{}.{}.{}.agg.nc'.format(
+        res, rec[:4], 'pp' if 'pp' in other else 'cp')
+    subtitle = '{} {} simulation {} precipitation reductions'.format(
+        res, rec.upper(), 'with' if 'pp' in other else 'without')
 
     # Load model output
     # -----------------
 
     # load output data (in the future combine='by_coords' will be the default)
-    print("loading " + ipath + "...")
-    ts = xr.open_mfdataset(
-        ipath+'/ts.???????.nc', combine='by_coords', decode_times=False)
-    ex = xr.open_mfdataset(
-        ipath+'/ex.???????.nc', combine='by_coords', decode_times=False,
-        chunks={'time': 50}, data_vars='minimal')
+    print("loading " + run_path + "...")
+    ex = xr.open_mfdataset(run_path+'/ex.???????.nc', decode_times=False,
+                           chunks=dict(time=50), combine='by_coords',
+                           data_vars='minimal')
 
     # get global attributes from last file (netcdf4 issue #835)
-    last = xr.open_dataset(ipath+'/ex.0120000.nc', decode_times=False)
+    last = xr.open_dataset(run_path+'/ex.0120000.nc', decode_times=False)
     ex.attrs = last.attrs
     last.close()
 
@@ -70,6 +77,7 @@ def postprocess(ipath, ofile, subtitle):
     #     long_name='last basal velocity x-component', units=ex.uvelbase.units)
     # pp['lastbvbvy'] = ex.vvelbase[-idx].where(idx > 0).assign_attrs(
     #     long_name='last basal velocity y-component', units=ex.vvelbase.units)
+    # pp.drop('time')
 
     # compute glacial cycle integrated variables
     pp['totalslip'] = dt*(ex.icy*ex.velbase_mag).sum(axis=0).assign_attrs(
@@ -80,7 +88,7 @@ def postprocess(ipath, ofile, subtitle):
         long_name='temperate-based ice cover duration', units='years')
 
     # add global attributes
-    pp.attrs.update(globs)
+    pp.attrs.update(GLOB_ATTRS)
     pp.attrs['subtitle'] = subtitle
     pp.attrs['history'] = pp.attrs['command'] + pp.attrs['history']
 
@@ -97,42 +105,23 @@ def postprocess(ipath, ofile, subtitle):
 
     # export to netcdf
     print("* exporting aggregated data...")
-    pp.drop('time')
-    pp.to_netcdf(ofile, mode='w',
+    pp.to_netcdf(out_file, mode='w',
                  encoding={var: {'zlib': True, 'shuffle': True, 'complevel': 5}
                            for var in pp.variables})
 
     # close datasets
     ex.close()
     pp.close()
-    ts.close()
 
 
 def main():
     """Main program called during execution."""
 
     # loop on selected runs
-    gridres = ['2km']*6 + ['1km']
-    records = ['grip']*2 + ['epica']*2 + ['md012444']*2 + ['epica']
-    pparams = ['cp', 'pp']*3 + ['pp']
-    offsets = [8.2, 10.4, 9.7, 12.2, 8.0, 10.6, 12.2]
-    for i in range(7):
-        res = gridres[i]
-        rec = records[i]
-        dp = pparams[i]
-        dt = offsets[i]
-        conf = ('.pp' if dp == 'pp' else '')
+    for run in PROC_RUNS:
 
         # input and output file paths
-        rname = 'alpcyc4.{}.{}.{:04.0f}{}'.format(res, rec, 100*dt, conf)
-        ipath = os.environ['HOME'] + '/pism/output/e9d2d1f/' + rname
-        ofile = 'processed/alpero.{}.{}.{}.agg.nc'.format(res, rec[:4], dp)
-        subtitle = ('{} {} simulation '.format(res, rec.upper()) +
-                    ('with ' if dp == 'pp' else 'without ') +
-                    'precipitation reductions')
-        subtitle = '{} {} simulation {} precipitation reductions'.format(
-            res, rec.upper(), 'with' if dp == 'pp' else 'without')
-        postprocess(ipath, ofile, subtitle)
+        postprocess(os.environ['HOME'] + '/pism/output/e9d2d1f/' + run)
 
 
 if __name__ == '__main__':
