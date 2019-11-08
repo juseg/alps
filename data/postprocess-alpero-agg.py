7c80dfbc1432aaaf5d7d12c19f0624fd4aa1e00a
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-11-08 14:29:42 +0100

    Begin to functionalize alpero postprocessing.

--- data/postprocess-alpero-agg.py
+++ data/postprocess-alpero-agg.py
@@ -26,30 +26,14 @@ globs = dict(
 # Loop on selected run
 # --------------------
 
-# loop on selected runs
-gridres = ['2km']*6 + ['1km']
-records = ['grip']*2 + ['epica']*2 + ['md012444']*2 + ['epica']
-pparams = ['cp', 'pp']*3 + ['pp']
-offsets = [8.2, 10.4, 9.7, 12.2, 8.0, 10.6, 12.2]
-for i in range(7):
-    res = gridres[i]
-    rec = records[i]
-    dp = pparams[i]
-    dt = offsets[i]
-    conf = ('.pp' if dp == 'pp' else '')
 
-    # input and output file paths
-    rname = 'alpcyc4.{}.{}.{:04.0f}{}'.format(res, rec, 100*dt, conf)
-    ipath = os.environ['HOME'] + '/pism/output/e9d2d1f/' + rname
-    ofile = 'processed/alpero.{}.{}.{}.agg.nc'.format(res, rec[:4], dp)
-    print(ipath)
-
+def postprocess(ipath, ofile, subtitle):
 
     # Load model output
     # -----------------
 
     # load output data (in the future combine='by_coords' will be the default)
-    print("loading " + rname + "...")
+    print("loading " + ipath + "...")
     ts = xr.open_mfdataset(
         ipath+'/ts.???????.nc', combine='by_coords', decode_times=False)
     ex = xr.open_mfdataset(
@@ -69,13 +53,12 @@ for i in range(7):
     # init postprocessed dataset with global attributes
     pp = xr.Dataset(attrs=ex.attrs, coords=dict(lon=ex.lon, lat=ex.lat))
 
-
     # Compute aggregated variables
     # ----------------------------
 
     # registering proxy variables
     ex['icy'] = (ex.thk >= 1.0)
-    ex['ero'] = 2.7e-7*(ex.icy*ex.velbase_mag)**2.02  # m/a (Herman et al, 2015)
+    ex['ero'] = 2.7e-7*(ex.icy*ex.velbase_mag)**2.02  # m/a (Herman etal, 2015)
     ex['warm'] = ex.icy*(ex.temppabase >= -1e-3)
 
     # compute index of last basal velocity
@@ -103,27 +86,27 @@ for i in range(7):
     ln = 'cumulative basal motion'
     print("* computing " + ln + "...")
     pp['totalslip'] = (ex.icy*ex.velbase_mag).sum(axis=0).compute()*dt
-    pp['totalslip'].attrs = dict(long_name=ln, grid_mapping='mapping', units='m')
+    pp['totalslip'].attrs = dict(long_name=ln, grid_mapping='mapping',
+                                 units='m')
     ln = 'cumulative glacial erosion'
     print("* computing " + ln + "...")
     pp['glerosion'] = ex.ero.sum(axis=0).compute()*dt
-    pp['glerosion'].attrs = dict(long_name=ln, grid_mapping='mapping', units='m year-1')
+    pp['glerosion'].attrs = dict(long_name=ln, grid_mapping='mapping',
+                                 units='m year-1')
     ln = 'temperate-based ice cover duration'
     print("* computing " + ln + "...")
     pp['warmcover'] = ex.warm.sum(axis=0).compute()*dt
-    pp['warmcover'].attrs = dict(long_name=ln, grid_mapping='mapping', units='years')
+    pp['warmcover'].attrs = dict(long_name=ln, grid_mapping='mapping',
+                                 units='years')
 
     # add global attributes
     pp.attrs.update(globs)
-    pp.attrs['subtitle'] = ('{} {} simulation '.format(res, rec.upper()) +
-                            ('with ' if dp == 'pp' else 'without ') +
-                            'precipitation reductions')
+    pp.attrs['subtitle'] = subtitle
     pp.attrs['history'] = pp.attrs['command'] + pp.attrs['history']
 
     # copy grid mapping and pism config
     pp['mapping'] = ex.mapping
     pp['pism_config'] = ex.pism_config
-
 
     # Export aggregated data
     # ----------------------
@@ -143,3 +126,34 @@ for i in range(7):
     ex.close()
     pp.close()
     ts.close()
+
+
+def main():
+    """Main program called during execution."""
+
+    # loop on selected runs
+    gridres = ['2km']*6 + ['1km']
+    records = ['grip']*2 + ['epica']*2 + ['md012444']*2 + ['epica']
+    pparams = ['cp', 'pp']*3 + ['pp']
+    offsets = [8.2, 10.4, 9.7, 12.2, 8.0, 10.6, 12.2]
+    for i in range(7):
+        res = gridres[i]
+        rec = records[i]
+        dp = pparams[i]
+        dt = offsets[i]
+        conf = ('.pp' if dp == 'pp' else '')
+
+        # input and output file paths
+        rname = 'alpcyc4.{}.{}.{:04.0f}{}'.format(res, rec, 100*dt, conf)
+        ipath = os.environ['HOME'] + '/pism/output/e9d2d1f/' + rname
+        ofile = 'processed/alpero.{}.{}.{}.agg.nc'.format(res, rec[:4], dp)
+        subtitle = ('{} {} simulation '.format(res, rec.upper()) +
+                    ('with ' if dp == 'pp' else 'without ') +
+                    'precipitation reductions')
+        subtitle = '{} {} simulation {} precipitation reductions'.format(
+            res, rec.upper(), 'with' if dp == 'pp' else 'without')
+        postprocess(ipath, ofile, subtitle)
+
+
+if __name__ == '__main__':
+    main()
