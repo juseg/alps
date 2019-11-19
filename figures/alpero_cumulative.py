e69d770b17490e3422107a31d6f41c39299b26c5
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-11-19 23:07:07 +0100

    Cleanuy cumulative plot with flake8 and pylint.

--- figures/alpero_cumulative.py
+++ figures/alpero_cumulative.py
@@ -3,36 +3,48 @@
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-import util as ut
+"""Plot Alps erosion time evolution."""
 
-# initialize figure
-fig, ax, cax, tsax = ut.fi.subplots_cax_ts()
+import util
 
-# load aggregated data
-# FIXME age coords in preprocessing, open with xarray
-with ut.io.open_dataset('../data/processed/alpero.1km.epic.pp.agg.nc') as ds:
 
-    # plot map data
-    glaciated = ds.cumu_erosion > 0.0
-    ds.cumu_erosion.where(glaciated).plot.contourf(
-        ax=ax, alpha=0.75, cmap='YlOrBr', levels=[10**i for i in range(0, 5)],
-        cbar_ax=cax, cbar_kwargs=dict(label='total erosion (m)', format='%g'))
-    glaciated.plot.contour(ax=ax, colors='k', linewidths=0.5, levels=[0.5])
+def main():
+    """Main program called during execution."""
 
-    # plot time series
-    # FIXME replace temperature offset by ice volume
-    data = ds.erosion_rate*1e-9
-    roll = data.rolling(time=100, center=True).mean()
-    twax = tsax.twinx()
-    twax.plot(data.age/1e3, data, c='C11', alpha=0.5)
-    twax.plot(data.age/1e3, roll, c='C11')
-    twax.set_ylabel(r'erosion rate ($km\,a^{-1}$)', color='C11')
-    twax.set_xlim(120.0, 0.0)
-    twax.set_ylim(-0.5, 3.5)
+    # initialize figure
+    fig, ax, cax, tsax = util.fi.subplots_cax_ts()
 
-# add map elements
-ut.pl.draw_boot_topo(ax)
-ut.ne.draw_natural_earth(ax)
+    # load aggregated data
+    # FIXME age coords in preprocessing, open with xarray
+    with util.io.open_dataset(
+            '../data/processed/alpero.1km.epic.pp.agg.nc') as ds:
 
-# save figure
-ut.pl.savefig()
+        # plot map data
+        glaciated = ds.cumu_erosion > 0.0
+        ds.cumu_erosion.where(glaciated).plot.contourf(
+            ax=ax, alpha=0.75, cmap='YlOrBr', cbar_ax=cax,
+            cbar_kwargs=dict(label='total erosion (m)', format='%g'),
+            levels=[10**i for i in range(0, 5)])
+        glaciated.plot.contour(ax=ax, colors='k', linewidths=0.5, levels=[0.5])
+
+        # plot time series
+        # FIXME replace temperature offset by ice volume
+        data = ds.erosion_rate*1e-9
+        roll = data.rolling(time=100, center=True).mean()
+        twax = tsax.twinx()
+        twax.plot(data.age/1e3, data, c='C11', alpha=0.5)
+        twax.plot(data.age/1e3, roll, c='C11')
+        twax.set_ylabel(r'erosion rate ($km\,a^{-1}$)', color='C11')
+        twax.set_xlim(120.0, 0.0)
+        twax.set_ylim(-0.5, 3.5)
+
+    # add map elements
+    util.pl.draw_boot_topo(ax)
+    util.ne.draw_natural_earth(ax)
+
+    # save figure
+    util.pl.savefig(fig)
+
+
+if __name__ == '__main__':
+    main()
