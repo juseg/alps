482e52f019b88cdaf34a805fd47dceb8a6bb9b12
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-11-19 22:49:57 +0100

    Use orange colors on cumulative erosion plot.

--- figures/alpero_cumulative.py
+++ figures/alpero_cumulative.py
@@ -15,17 +15,18 @@ with ut.io.open_dataset('../data/processed/alpero.1km.epic.pp.agg.nc') as ds:
     # plot map data
     glaciated = ds.cumu_erosion > 0.0
     ds.cumu_erosion.where(glaciated).plot.contourf(
-        ax=ax, alpha=0.75, cmap='magma_r', levels=[10**i for i in range(0, 5)],
+        ax=ax, alpha=0.75, cmap='YlOrBr', levels=[10**i for i in range(0, 5)],
         cbar_ax=cax, cbar_kwargs=dict(label='total erosion (m)', format='%g'))
     glaciated.plot.contour(ax=ax, colors='k', linewidths=0.5, levels=[0.5])
 
     # plot time series
     # FIXME replace temperature offset by ice volume
+    data = ds.erosion_rate*1e-9
+    roll = data.rolling(time=100, center=True).mean()
     twax = tsax.twinx()
-    twax.plot(ds.age/1e3, ds.erosion_rate*1e-9, c='C8', alpha=0.75)
-    twax.plot(ds.age/1e3, ds.erosion_rate.rolling(
-        time=100, center=True).mean()*1e-9, c='C9')
-    twax.set_ylabel(r'erosion rate ($km\,a^{-1}$)', color='C9')
+    twax.plot(data.age/1e3, data, c='C11', alpha=0.5)
+    twax.plot(data.age/1e3, roll, c='C11')
+    twax.set_ylabel(r'erosion rate ($km\,a^{-1}$)', color='C11')
     twax.set_xlim(120.0, 0.0)
     twax.set_ylim(-0.5, 3.5)
 
