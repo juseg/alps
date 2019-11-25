23b272166e6731354b79f029c17fe4b3c2d28359
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-11-25 17:06:52 +0100

    Add boot hypsometry in scatter plot animation.

--- movies/anim_alps_dots.py
+++ movies/anim_alps_dots.py
@@ -5,6 +5,7 @@
 
 """Alps scatter plot animations framework."""
 
+import numpy as np
 import matplotlib.animation as animation
 import absplots as apl
 import utils as ut
@@ -16,24 +17,34 @@ def figure():
     """Prepare initial animation figure."""
 
     # initialize figure
-    fig, ax = apl.subplots_mm(figsize=(192, 108), gridspec_kw=dict(
-        left=12, right=4, bottom=12, top=4))
+    fig, grid = apl.subplots_mm(
+        figsize=(192, 108), ncols=2, sharey=True, gridspec_kw=dict(
+            left=12, right=4, bottom=8, top=8, width_ratios=[3, 1]))
 
     # load boot topo
     with ut.open_dataset('../data/processed/alpcyc.1km.in.nc') as ds:
         boot = ds.topg
 
     # plot dummy scatter and age tag
+    ax = grid[0]
     scatter = ax.scatter(0*boot, boot, alpha=0.05, color='C11', marker='.')
     timetag = ax.text(0.95, 0.95, '', ha='right', va='top',
                       transform=ax.transAxes)
 
     # set axes properties
     ax.set_xscale('log')
-    ax.set_xlim(1e-16, 1e1)
+    ax.set_xlim(1e-9/9, 9*1e0)
     ax.set_ylim(-200, 4700)
+    ax.set_xticks(np.logspace(-9, 0, 10))
     ax.set_xlabel(r'erosion rate ($m\,a^{-1}$)')
     ax.set_ylabel('elevation (m)')
+
+    # plot boot hypsometry
+    ax = grid[1]
+    bins = range(0, 4501, 100)
+    ax.hist(boot.where(boot > 0).values.reshape(-1), bins=bins,
+            color='0.75', orientation='horizontal')
+    ax.set_xticks([])
 
     # return figure and animated artists
     return fig, scatter, timetag
@@ -63,9 +74,12 @@ def main():
     """Main program called during execution."""
 
     # prepare figure
-    fig, scatter, timetag = figure()
+    fig, *fargs = figure()
+    animate(-25000, *fargs)
+    fig.savefig('anim_alps_dots')
 
     # animation
+    # FIXME can't use blit with histogram
     ani = animation.FuncAnimation(
         fig, animate, blit=True, interval=1000/25, fargs=(scatter, timetag),
         frames=range(-119000, 1, 400))
