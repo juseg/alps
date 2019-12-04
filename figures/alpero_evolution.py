a81f78656fc1984232c9433ef1be452e6ecb55ef
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-12-04 13:57:03 +0100

    Animate erosion evolution plot (wip).

--- figures/alpero_evolution.py
+++ figures/alpero_evolution.py
@@ -10,8 +10,8 @@ import absplots as apl
 import util
 
 
-def main():
-    """Main program called during execution."""
+def figure():
+    """Prepare initial animation figure."""
 
     # initialize figure
     fig, ax = apl.subplots_mm(figsize=(177, 119), gridspec_kw=dict(
@@ -21,18 +21,24 @@ def main():
     with xr.open_mfdataset([
             '../data/processed/alpero.1km.epic.pp.agg.nc',
             '../data/processed/alpcyc.1km.epic.pp.ts.10a.nc'],
-                           combine='by_coords', join='override') as ds:
-        eros = ds.erosion_rate*1e-9
-        roll = eros.rolling(time=100, center=True).mean()
-        slvol = ds.slvol*100
+                           combine='by_coords', decode_cf=False,
+                           join='override') as data:
+        data = data[['slvol', 'erosion_rate']]
+        data['time'] /= 365*24*60*60
+        data['slvol'] *= 100
+        data['erosion_rate'] *= 1e-9
+        data['rolling_mean'] = data.erosion_rate.rolling(
+            time=100, center=True).mean()
 
     # for volume changes use
     # slvol = slvol.rolling(time=100, center=True).mean()
     # slvol = slvol.differentiate('time')
 
     # plot
-    ax.plot(slvol, eros, c='C11', alpha=0.5)
-    ax.plot(slvol, roll, c='C11')
+    line0, = ax.plot(data.slvol, data.erosion_rate, c='C11', alpha=0.5)
+    line1, = ax.plot(data.slvol, data.rolling_mean, c='C11')
+    timetag = ax.text(0.95, 0.95, '', ha='right', va='top',
+                      transform=ax.transAxes)
 
     # set axes properties
     ax.set_xlabel('ice volume (cm s.l.e.)')
@@ -40,8 +46,39 @@ def main():
     ax.set_yscale('log')
     ax.set_ylim(0.8e-2, 1.2e1)
 
-    # save figure
+    # return figure, data and animated artists
+    return fig, data, line0, line1, timetag
+
+
+def animate(time, data, line0, line1, timetag):
+    """Update figure data."""
+
+    # replace line data
+    line0.set_ydata(data.erosion_rate.where(data.time <= time))
+    line1.set_ydata(data.rolling_mean.where(data.time <= time))
+
+    # replace text tag
+    timetag.set_text('{:,.0f} years ago'.format(-time).replace(',', r'$\,$'))
+
+    # return animated artists
+    return line0, line1, timetag
+
+
+def main():
+    """Main program called during execution."""
+
+    # prepare figure
+    fig, *fargs = figure()
     util.pl.savefig(fig)
+
+    # prepare animation
+    # import os
+    # import sys
+    # import matplotlib.animation as animation
+    # ani = animation.FuncAnimation(
+    #     fig, animate, blit=True, interval=1000/25, fargs=fargs,
+    #     frames=range(-119000, 1, 40))
+    # ani.save(os.path.splitext(sys.argv[0])[0] + '.mp4')
 
 
 if __name__ == '__main__':
