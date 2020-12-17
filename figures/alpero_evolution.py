d5a66e39074608251b73104972bcad24aaa94b8c
Julien Seguinot <juseg@posteo.eu>
2020-12-17 13:25:48 +0100

    Fix opening datasets in alpero evolution fig.

--- figures/alpero_evolution.py
+++ figures/alpero_evolution.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-# Copyright (c) 2019, Julien Seguinot (juseg.github.io)
+# Copyright (c) 2019-2020, Julien Seguinot (juseg.github.io)
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
 """Plot Alps erosion time evolution."""
 
-import xarray as xr
+import pismx.open
 import absplots as apl
 import util
 
@@ -18,25 +18,21 @@ def figure():
         left=12, right=1.5, bottom=12, top=1.5))
 
     # load postprocessed data
-    with xr.open_mfdataset([
-            '../data/processed/alpero.1km.epic.pp.agg.nc',
-            '../data/processed/alpcyc.1km.epic.pp.ts.10a.nc'],
-                           combine='by_coords', decode_cf=False,
-                           join='override') as data:
-        data = data[['slvol', 'erosion_rate']]
-        data['time'] /= 365*24*60*60
-        data['slvol'] *= 100
-        data['erosion_rate'] *= 1e-9
-        data['rolling_mean'] = data.erosion_rate.rolling(
-            time=100, center=True).mean()
+    with pismx.open.dataset(
+            '../data/processed/alpcyc.1km.epic.pp.ts.10a.nc') as ds:
+        ds = ds[['slvol']]
+    with pismx.open.dataset(
+            '../data/processed/alpero.1km.epic.pp.agg.nc') as agg:
+        ds['erosion_rate'] = agg.erosion_rate
 
-    # for volume changes use
-    # slvol = slvol.rolling(time=100, center=True).mean()
-    # slvol = slvol.differentiate('time')
+    # unit conversion and rolling mean
+    ds['slvol'] *= 100
+    ds['erosion_rate'] *= 1e-9
+    ds['rolling_mean'] = ds.erosion_rate.rolling(age=100, center=True).mean()
 
     # plot
-    line0, = ax.plot(data.slvol, data.erosion_rate, c='C11', alpha=0.5)
-    line1, = ax.plot(data.slvol, data.rolling_mean, c='C11')
+    line0, = ax.plot(ds.slvol, ds.erosion_rate, c='C11', alpha=0.5)
+    line1, = ax.plot(ds.slvol, ds.rolling_mean, c='C11')
     timetag = ax.text(0.95, 0.95, '', ha='right', va='top',
                       transform=ax.transAxes)
 
@@ -47,15 +43,15 @@ def figure():
     ax.set_ylim(0.8e-2, 1.2e1)
 
     # return figure, data and animated artists
-    return fig, data, line0, line1, timetag
+    return fig, ds, line0, line1, timetag
 
 
-def animate(time, data, line0, line1, timetag):
+def animate(time, ds, line0, line1, timetag):
     """Update figure data."""
 
     # replace line data
-    line0.set_ydata(data.erosion_rate.where(data.time <= time))
-    line1.set_ydata(data.rolling_mean.where(data.time <= time))
+    line0.set_ydata(ds.erosion_rate.where(ds.time <= time))
+    line1.set_ydata(ds.rolling_mean.where(ds.time <= time))
 
     # replace text tag
     timetag.set_text('{:,.0f} years ago'.format(-time).replace(',', r'$\,$'))
@@ -72,6 +68,7 @@ def main():
     util.com.savefig(fig)
 
     # prepare animation
+    # FIXME prepare a fancy animation in separate script
     # import os
     # import sys
     # import matplotlib.animation as animation
