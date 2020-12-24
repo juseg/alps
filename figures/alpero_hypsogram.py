b600ea6f5a23ddf1a2376e1b9e681f8a783e5f0e
Julien Seguinot <juseg@posteo.eu>
2020-12-24 11:17:35 +0100

    Add boot hypsometry on hypsogram figure.

--- figures/alpero_hypsogram.py
+++ figures/alpero_hypsogram.py
@@ -15,11 +15,16 @@ def main():
     """Main program called during execution."""
 
     # initialize figure
-    fig, (ax, tsax) = apl.subplots_mm(
-        figsize=(177, 80), nrows=2, sharex=True, gridspec_kw=dict(
-            left=12, right=18, bottom=9, top=1.5,
-            hspace=1.5, height_ratios=(48, 20)))
-    cax = fig.add_axes_mm([177-18+1.5, 9+20+1.5, 3, 48])
+    fig = apl.figure_mm(figsize=(177, 80))
+    spec = fig.add_gridspec_mm(
+        ncols=2, nrows=2, left=12, right=1.5, bottom=9, top=1.5, hspace=1.5,
+        wspace=1.5, height_ratios=(48, 20), width_ratios=(142, 20))
+    ax = fig.add_subplot(spec[0, 0])
+    hax = fig.add_subplot(spec[0, 1], sharey=ax)
+    tsax = fig.add_subplot(spec[1, 0], sharex=ax)
+    cax = fig.add_axes_mm([177-18+1.5, 9+20+1.5+24, 3, 24])
+
+    # add subfigure labels
     util.fig.add_subfig_label('(a)', ax=ax)
     util.fig.add_subfig_label('(b)', ax=tsax)
 
@@ -31,19 +36,37 @@ def main():
         np.log10(ds.her2015_hyps).plot.imshow(
             ax=ax, alpha=0.75, cmap='YlOrBr', vmin=-9, vmax=0, x='age',
             cbar_ax=cax, cbar_kwargs=dict(
-                label='log10 geometric mean\n'+r'erosion rate ($m\,a^{-1}$)'))
+                label='log10 geometric mean\n'+r'erosion rate ($m\,a^{-1}$)',
+                ticks=range(-9, 1, 3)))
         # this should work in matplotlib 3.3.2 (PR #18458)
         # ds.her2015_hyps.plot.imshow(
         #    ax=ax, alpha=0.75, cmap='YlOrBr', norm=mcolors.LogNorm(1e-9, 1e0),
         #    x='age', cbar_ax=cax, cbar_kwargs=dict(
         #        label=r'erosion rate ($m\,a^{-1}$)'))
         ax.set_xlabel('')
+        ax.set_ylabel('')
         ax.set_ylabel('elevation (m)')
+        ax.tick_params(labelbottom=False)
+
+    # plot boot hypsometry
+    with pismx.open.dataset('../data/processed/alpcyc.1km.in.nc') as ds:
+
+        # plot boot hypsometry
+        bins = np.arange(0, 4501, 100)
+        hist, _ = np.histogram(ds.topg.where(ds.topg > 0), bins=bins)
+        vals = np.append(hist, hist[-1])  # needed to fill the last bin
+        hax.fill_betweenx(bins, 0*vals, vals, color='0.25', step='post')
+        hax.grid(False)
+        hax.set_frame_on(False)
+        hax.set_xlim(0, 60000)
+        hax.set_xticks([])
+        hax.tick_params(labelleft=False)
 
     # plot time series
     util.fig.plot_mis(ax=ax, y=None)
     util.fig.plot_mis(ax=tsax, y=0.9)
     util.ero.plot_series(ax=tsax)
+    tsax.set_ylabel('ice volume\n(cm s.l.e.)')
 
     # save figure
     util.com.savefig(fig)
