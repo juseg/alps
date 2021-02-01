3fc3305c892af27e34fee3756ee5baf16809339f
Julien Seguinot <juseg@posteo.eu>
2021-02-01 15:42:35 +0100

    Rearrage erosion laws figure for clarity.

--- figures/alpero_powerlaws.py
+++ figures/alpero_powerlaws.py
@@ -12,20 +12,16 @@ import absplots as apl
 import pismx.open
 import util
 
-DASHES = dict(
-    kop2015=(), her2015=(3, 1, 1, 1), hum1994=(2, 1), coo2020=(1, 1))
-
 
 def plot_erosion_laws(ax):
     """Plot erosion power laws."""
 
     # plot power laws (in m/a)
     sliding = np.logspace(0, 4, 5)
-    ax.plot(sliding, 5.2e-11*sliding**2.34, c='C11', dashes=DASHES['kop2015'])
-    ax.plot(sliding, 2.7e-7*sliding**2.02, c='C11', dashes=DASHES['her2015'])
-    ax.plot(sliding, 1e-4*sliding, c='C11', dashes=DASHES['hum1994'])
-    ax.plot(sliding, 1.665e-4*sliding**0.6459, c='C11',
-            dashes=DASHES['coo2020'])
+    ax.plot(sliding, 5.2e-11*sliding**2.34, c='C11')
+    ax.plot(sliding, 2.7e-7*sliding**2.02, c='C11')
+    ax.plot(sliding, 1e-4*sliding, c='C11')
+    ax.plot(sliding, 1.665e-4*sliding**0.6459, c='C11')
 
     # set axes properties
     ax.set_xscale('log')
@@ -40,16 +36,16 @@ def main():
 
     # initialize figure
     fig = apl.figure_mm(figsize=(177, 119))
-    plax = fig.add_axes_mm([15, 119-1.5-30, 39, 30])
-    pfax = fig.add_axes_mm([15+39+15, 119-1.5-30, 177-15-39-15-18, 30])
     grid = fig.subplots_mm(
         ncols=4, subplot_kw=dict(projection=ccrs.UTM(32)), gridspec_kw=dict(
-            left=15, right=1.5, bottom=38.5, top=41.5, wspace=1.5))
-    tsgrid = fig.subplots_mm(ncols=4, sharey=True, gridspec_kw=dict(
-        left=15, right=1.5, bottom=9, top=82, wspace=1.5))
+            left=15, right=1.5, bottom=75.5, top=4.5, wspace=1.5))
+    tsgrid = fig.subplots_mm(
+        ncols=4, nrows=2, sharex='row', sharey='row', gridspec_kw=dict(
+            left=15, right=1.5, bottom=9, top=45, hspace=9, wspace=1.5))
+    cax = fig.add_axes_mm([10.5, 75.5, 3, 39])
 
     # set extent and subfig labels
-    for ax, label in zip([plax]+[pfax]+list(grid)+list(tsgrid), 'abcdefghij'):
+    for ax, label in zip(list(grid)+list(tsgrid.flat), 'abcdefghijkl'):
         util.fig.add_subfig_label('(%s)' % label, ax=ax)
     for ax in grid:
         util.fig.prepare_map_axes(ax)
@@ -59,9 +55,6 @@ def main():
     with pismx.open.dataset(
             '../data/processed/alpcyc.1km.epic.pp.ts.10a.nc') as ds:
         slvol = ds.slvol
-
-    # plot erosion laws
-    plot_erosion_laws(plax)
 
     # read profile coords in km
     x, y = cpf.read_shp_coords('../data/native/profile_rhine.shp')
@@ -73,7 +66,8 @@ def main():
                 '../data/processed/alpero.1km.epic.pp.agg.nc') as ds:
 
         # for each erosion law
-        for ax, tsax, ref in zip(grid, tsgrid, DASHES):
+        for ax, pfax, tsax, ref in zip(
+                grid, *tsgrid, ['kop2015', 'her2015', 'hum1994', 'coo2020']):
 
             # plot cumulative erosion
             cset = ds[ref+'_cumu'].plot.contourf(
@@ -89,31 +83,36 @@ def main():
             ax.plot(x, y, color='0.25', dashes=(2, 1))
             ax.plot(x[0], y[0], color='0.25', marker='o')
 
+            # authors name as title
+            ax.set_title(ds[ref+'_cumu'].long_name.split(')')[0] + ')')
+
+            # plot interpolated profile
+            ds[ref+'_cumu'].interp(x=x, y=y, method='linear').plot(
+                ax=pfax, color='C11')
+
+            # set profile axes properties
+            pfax.set_xlabel('distance along flow (km)')
+            pfax.set_ylabel('')
+            pfax.set_yscale('log')
+
             # plot time series
             tsax.plot(slvol, ds[ref+'_rate'], c='C11', alpha=0.5)
             tsax.plot(slvol, ds[ref+'_rate'].rolling(
                 age=100, center=True).mean(), c='C11')
 
-            # plot interpolated profile
-            interp = ds[ref+'_cumu'].interp(x=x, y=y, method='linear')
-            interp.plot(ax=pfax, color='C11', dashes=DASHES[ref],
-                        label=ds[ref+'_cumu'].long_name.split(')')[0] + ')')
-
             # set time series axes properties
             tsax.set_xlabel('ice volume (cm s.l.e.)')
             tsax.set_yscale('log')
-            tsax.set_ylim(10**4.3, 10**7.7*1e3)
+            tsax.set_ylabel('')
+            tsax.set_ylim(10**4.5, 10**10.5)
 
-    # set profile axes properties
-    tsgrid[0].set_ylabel(r'volumic erosion rate ($m^3\,a^{-1}$)')
-    pfax.set_xlabel('distance along flow (km)')
-    pfax.set_ylabel('erosion potential (m)')
-    pfax.set_yscale('log')
-    pfax.legend()
+    # add y-labels on leftmost axes
+    tsgrid[0, 0].set_ylabel('erosion potential (m)')
+    tsgrid[1, 0].set_ylabel(r'volumic erosion rate ($m^3\,a^{-1}$)')
 
     # add colorbar
-    fig.colorbar(cset, cax=fig.add_axes_mm([177-18+1.5, 119-1.5-30, 3, 30]),
-                 label='erosion potential (m)', format='%g')
+    fig.colorbar(cset, cax=cax, format='%g', pad=-10)
+    cax.yaxis.set_ticks_position('left')
 
     # save
     util.com.savefig(fig)
