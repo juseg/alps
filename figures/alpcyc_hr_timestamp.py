e6ed9ec16d1213e537e85201063f5dbef8cb1152
Julien Seguinot <juseg@posteo.eu>
2021-05-16 11:28:27 +0200

    Commit updated and improved timestamps figure.

--- figures/alpcyc_hr_timestamp.py
+++ figures/alpcyc_hr_timestamp.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python
-# coding: utf-8
+# Copyright (c) 2015-2021, Julien Seguinot (juseg.github.io)
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
+
+"""Alps cycle hi-res computing time stamps."""
 
 import hyoga.open
 import util
-import numpy as np
-import xarray as xr
 
-# initialize figure
-fig, ax = util.fig.subplots_ts()
 
-# plot cumulative time stamp
-with pismx.open.dataset('../data/processed/alpcyc.1km.epic.pp.tms.nc') as ds:
-    ts = ds.timestamp
-    dt = ts.diff('age')
-    ts = dt.where(dt > 0.0, ts[1:]).cumsum()/24.0
-    ax.plot(ts.age, ts, color='C1')
+def main():
+    """Main program called during execution."""
 
-# set axes properies
-ax.invert_xaxis()
-ax.set_xlabel('model time (ka)')
-ax.set_ylabel('computing time (days)')
+    # initialize figure
+    fig, ax = util.fig.subplots_ts()
 
-# save
-util.com.savefig()
+    # plot cumulative time stamp
+    with hyoga.open.dataset(
+            '../data/processed/alpcyc.1km.epic.pp.tms.nc') as ds:
+        diff = ds.timestamp.diff('age')
+        procs = 576  # ds['rank'].max() + 1
+        stamp = diff.where(diff > 0, ds.timestamp[1:]).cumsum()
+        stamp.plot(ax=ax, color='C1', label=r'{:d} cores, {:.0f} kch'.format(
+            int(procs), float(procs*stamp[-1]/1e3)))
+
+    # set axes properies
+    ax.invert_xaxis()
+    ax.set_xlabel('model age (ka)')
+    ax.set_ylabel('computing time (hours)')
+    ax.legend(loc='best')
+
+    # save
+    fig.savefig(__file__[:-3])
+
+
+if __name__ == '__main__':
+    main()
